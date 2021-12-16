e09773b15f92f8773c349b4e13299021bc3573dc
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-12-16 21:08:11 +0100

    Setup Eslint and upgrade packages

--- stories/Header.stories.tsx
+++ stories/Header.stories.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { Header } from './Header';
+import { Header } from "./Header";
 
 export default {
-  title: 'Example/Header',
+  title: "Example/Header",
   component: Header,
 } as ComponentMeta<typeof Header>;
 
 const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
 
 export const LoggedIn = Template.bind({});
+
 LoggedIn.args = {
   user: {},
 };
