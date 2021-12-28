85b86e8b43cd6033709028f53cd4f31e66ac4125
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-12-28 01:45:18 +0100

    Fjern Cypress standard tester

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -2,16 +2,25 @@
  * @jest-environment jsdom
  */
 
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Home from '../pages/index';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
 
-describe('Home', () => {
-  it('renders a heading', () => {
+describe("Home", () => {
+  it("Welcome to next eksisterer ikke", () => {
     render(<Home />);
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
+    const welcometoNext = screen.queryByText("heading", {
+      name: /welcome to next/i,
     });
-    expect(heading).toBeInTheDocument();
+    console.log(welcometoNext);
+    expect(welcometoNext).not.toBeInTheDocument();
+  });
+
+  it("Velkommen til Index eksisterer", () => {
+    render(<Home />);
+    const velkommen = screen.getByRole("heading", {
+      name: /velkommen til Index/i,
+    });
+    expect(velkommen).toBeInTheDocument();
   });
 });
