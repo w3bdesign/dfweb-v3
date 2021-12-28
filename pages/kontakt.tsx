19808da9af0fa0e585e157366c65b6e9004d5b72
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-12-28 01:54:58 +0100

    Fjern  ubrukte imports

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+
 import styles from "../styles/Home.module.css";
 
 const Kontakt: NextPage = () => {
