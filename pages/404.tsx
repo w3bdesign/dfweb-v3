c11b9238dc175c295d9c5457db41720010148728
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-12-28 01:24:50 +0100

    Begynn arbeid med sider

--- pages/404.tsx
+++ pages/404.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-        Velkommen til Index
+        Velkommen til 404
         </h1>
 
         <p className={styles.description}>
--- pages/prosjekter.tsx
+++ pages/prosjekter.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-        Velkommen til Index
+         Velkommen til Prosjekter
         </h1>
 
         <p className={styles.description}>
