5b295f09c8c9c94ff03c80fe390aa541f4cbbcb1
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-12-29 00:54:18 +0100

    Update index.spec.ts

--- cypress/integration/index.spec.ts
+++ cypress/integration/index.spec.ts
@@ -1,15 +1,17 @@
 /// <reference types="cypress"/>
 
+// import cy from "cypress";
+
 describe(`The Home Page`, () => {
   beforeEach(() => {
     cy.visit("/");
   });
   it(`Contains my name in the hero section`, () => {
-   cy.get("h1").contains("Velkommen");
+    cy.get("h1").contains("Velkommen");
   });
 });
 
-export {}
+export {};
 
 /*context("Home", () => {
   beforeEach(() => {
