45371cf05afa104d86e3fb4866cd887d6cbe7d05
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-12-29 00:04:43 +0100

    Update index.spec.js

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,19 +1,10 @@
-/* global cy, describe, beforeEach, it */ 
+/* global cy, describe, beforeEach, it */
 
-import cy from "cypress"
-
-describe('Navigation', () => {
-    it('should navigate to the about page', () => {
-      // Start from the index page
-      cy.visit('http://localhost:3000/')
-  
-      // Find a link with an href attribute containing "about" and click it
-      //cy.get('a[href*="about"]').click()
-  
-      // The new url should include "/about"
-      //cy.url().should('include', '/about')
-  
-      // The new page should contain an h1 with "About page"
-      cy.get('h1').contains('Velkommen')
-    })
-  })
+describe(`The Home Page`, () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it(`Contains my name in the hero section`, () => {
+    cy.get("[data-cy=daniel]").not.contains(`Daniel`);
+  });
+});
