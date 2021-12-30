b53b2c977d55613f7987bc51d8e4196322699b9b
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-12-30 03:59:09 +0100

    Fiks Typescript feil

--- components/Layout/Header.component.tsx
+++ components/Layout/Header.component.tsx
@@ -1,5 +1,5 @@
-import { NextPage } from "next";
-import Head from "next/head";
+import { NextPage } from "Next";
+import Head from "Next/head";
 
 import styles from "../../styles/Home.module.css";
 
