ea9bc605650ca2dc49e11f070cfca81e828ffcbe
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-12-30 04:48:33 +0100

    Legg til interessant info om Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
 // https://dev.to/jonasmerlin/how-to-use-the-next-js-image-component-in-storybook-1415
+// https://www.youtube.com/watch?v=i5tvZ9f7gJw
+
 import "../styles/globals.css";
 
 export const parameters = {
