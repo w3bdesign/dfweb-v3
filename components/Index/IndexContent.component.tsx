aee0f631b42f15ae300efd171838361e307a286c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-01 03:52:41 +0100

    Oppdater pakker

--- components/Index/IndexContent.component.tsx
+++ components/Index/IndexContent.component.tsx
@@ -6,7 +6,7 @@ const IndexContent: NextComponentType = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className="text-5xl text-rose-500">Velkommen til Index</h1>
+        <h1 className="text-5xl text-rose-500 text-center">Velkommen til Index denne er i komponenten</h1>
       </main>
     </div>
   );
