f60f076a1885a37a4594def4686a4b86a365b1f1
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-01 23:14:18 +0100

    Fjern ubrukt modul-CSS fil

--- components/Prosjekter/ProsjekterContent.component.tsx
+++ components/Prosjekter/ProsjekterContent.component.tsx
@@ -1,12 +1,10 @@
 import type { NextComponentType } from "next";
 
-import styles from "../../styles/Home.module.css";
-
 const ProsjekterContent: NextComponentType = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Velkommen til Prosjekter</h1>
+    <div>
+      <main>
+        <h1>Velkommen til Prosjekter</h1>
       </main>
     </div>
   );
