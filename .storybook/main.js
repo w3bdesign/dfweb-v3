e9c1eb838f3960bc6e65f6850fc934874adbad5e
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-02 02:45:15 +0100

    Fullfør oppsett av Tailwind med Storybook
    Vi trenger PostCSS

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,19 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss")
+        }
+      }
+    }
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5"
   }
-}
+};
