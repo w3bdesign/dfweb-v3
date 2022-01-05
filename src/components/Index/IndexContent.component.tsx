5bea6948951c908db7e5e5f11917094055e1e88a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-05 04:56:59 +0100

    Fjern code smell

--- src/components/Index/IndexContent.component.tsx
+++ src/components/Index/IndexContent.component.tsx
@@ -2,10 +2,8 @@ import Link from "next/link";
 import PortableText from "react-portable-text";
 
 import type { NextComponentType } from "next";
-import React from "react";
 
 const IndexContent: NextComponentType = ({ post }: any) => {
-
   return (
     <main role="main" aria-label="Her kommer hovedinnholdet" id="maincontent">
       <div className="mx-auto mt-16 rounded lg:mt-20 xl:mt-20 bg-graybg shadow-large md:mt-16 sm:mt-64 xs:mt-64">
@@ -23,13 +21,13 @@ const IndexContent: NextComponentType = ({ post }: any) => {
           </div>
         </div>
         <div className="container grid gap-4 p-4 mx-auto mt-2 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 xs:grid-cols-1">
-          {post.map((post: any) => (
-            <div className="mt-4 p-8 text-lg text-black bg-white rounded shadow" key={post._id}>
-              <section role="contentinfo" aria-label={post.title}>
-                <h2 className="text-3xl text-center">{post.title}</h2>
+          {post.map((content: any) => (
+            <div className="mt-4 p-8 text-lg text-black bg-white rounded shadow" key={content._id}>
+              <section role="contentinfo" aria-label={content.title}>
+                <h2 className="text-3xl text-center">{content.title}</h2>
                 <PortableText
                   className="text-xl"
-                  content={post.text}
+                  content={content.text}
                   serializers={{
                     code: (props: any) => <p className="mt-6 text-lg">{props.children} </p>,
                     link: (props: any) => (
