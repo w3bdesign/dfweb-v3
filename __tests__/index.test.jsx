e27eab41cc603c4490643e29848af4c00c5d5538
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-05 23:27:43 +0100

    Fiks tester

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -2,26 +2,25 @@
  * @jest-environment jsdom
  */
 
- import React from "react";
- import { render, screen } from "@testing-library/react";
- import Home from "../src/pages/index";
- 
- describe("Home", () => {
-   it("Welcome to next eksisterer ikke", () => {
-     render(<Home />);
-     const welcometoNext = screen.queryByText("heading", {
-       name: /welcome to next/i,
-     });
-  
-     expect(welcometoNext).not.toBeInTheDocument();
-   });
- 
-   it("Om Meg eksisterer", () => {
-     render(<Home />);
-     const velkommen = screen.getByRole("heading", {
-       name: /Om Meg/i,
-     });
-     expect(velkommen).toBeInTheDocument();
-   });
- });
- 
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Home from "../src/pages/index";
+
+describe("Home", () => {
+  it("Welcome to next eksisterer ikke", () => {
+    render(<Home />);
+    const welcometoNext = screen.queryByText("heading", {
+      name: /welcome to next/i
+    });
+
+    expect(welcometoNext).not.toBeInTheDocument();
+  });
+
+  it("Om Meg eksisterer", () => {
+    render(<Home />);
+    const banner = screen.getByRole("banner", {
+      name: /Header for logo og navigasjon/i
+    });
+    expect(banner).toBeInTheDocument();
+  });
+});
