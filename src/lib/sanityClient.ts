43afde3bcdb8c404278c78e61988e3e30728ccaa
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-05 05:52:16 +0100

    Update sanityClient.ts

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,5 +1,7 @@
 import sanityClient from "@sanity/client";
 
+// TODO Vi trenger ikke denne mer?
+
 export default sanityClient({
   projectId: "41s7iutf", // you can find this in sanity.json
   dataset: "production", // or the name you chose in step 1
