7087e2a4d85e27f416c1537be77f9895944439f3
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-08 04:58:41 +0100

    Påbegynn arbeid med prosjektvisning

--- src/components/Prosjekter/ProsjekterContent.component.tsx
+++ src/components/Prosjekter/ProsjekterContent.component.tsx
@@ -45,6 +45,114 @@ const ProsjekterContent: NextPage<IProject> = ({ project }) => {
           <li>{post.urlwww}</li>
         </div>
       ))}
+
+
+
+
+
+
+
+
+
+return (
+    <main aria-label="Innhold portefølje" className="mt-32 bg-graybg">
+      <div className="container mx-auto rounded">
+        <div className="px-4 mx-auto mt-4 lg:px-0 xl:px-0 md:px-0">
+          <span className="flex justify-end mb-4">
+            <form>
+              <label
+                className="p-2 mr-4 text-lg"
+                aria-label="kategorifilter"
+                htmlFor="kategorifilter"
+              >
+                Filtrer kategori:
+              </label>
+              <select
+                id="kategorifilter"
+                name="kategorifilter"
+                //onChange={handleFilterChange}
+                className="w-40 p-2 leading-tight text-black border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              >
+                <option label="" value="">
+                  Ingen filtrering
+                </option>
+                {/*CATEGORIES &&
+                  CATEGORIES.map(({ id, name }) => (
+                    <option key={id} value={name}>
+                      {name}
+                    </option>
+                  ))*/}
+              </select>
+            </form>
+          </span>
+
+
+
+        
+
+          <ProsjektProjects
+                        // filter={categoryFilter}
+                        projects={project}
+                      />
+        </div>
+      </div>
+    </main>
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </>
   );
 };
