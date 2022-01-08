fd0a1b80171b41d93743611f4401ebf56e03e5b0
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-08 19:27:37 +0100

    Fjern ubrukt kode

--- src/components/Prosjekter/ProsjektIndividualProjects.component.tsx
+++ src/components/Prosjekter/ProsjektIndividualProjects.component.tsx
@@ -1,32 +1,3 @@
-/*
-  return (
-    <>
-      <h1 className="text-5xl text-rose-500 text-center">
-        Velkommen til Prosjekter denne er fra komponenten
-      </h1>
-      {project.map((post: IProjectInterface) => (
-        <div className="mt-24" key={post._id}>
-          <li>{post.id}</li>
-          <li>{post.name}</li>
-          <li>{post.description}</li>
-          <li>{post.subdescription}</li>
-          <li>
-            {post.projectimage && (
-              <Image
-                height="255"
-                width="500"
-                src={urlFor(post.projectimage).url() as string}
-                alt="Test"
-              />
-            )}
-          </li>
-          <li>{post.urlgithub}</li>
-          <li>{post.urlwww}</li>
-        </div>
-      ))}
-}
-*/
-
 import Image from "next/image";
 
 import { urlFor } from "../../lib/sanity";
