956d03245b1f0bd46e4a9bcbb07806d9d6e6f0d4
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-14 01:59:06 +0100

    Fiks "Fix '&quot; can be escaped"

--- stories/Page.tsx
+++ stories/Page.tsx
@@ -31,7 +31,7 @@ export const Page = ({ user, onLogin, onLogout, onCreateAccount }: IPageProps) =
       <ul>
         <li>
           Use a higher-level connected component. Storybook helps you compose such data from the
-          "args" of child component stories
+          arguments of child component stories
         </li>
         <li>
           Assemble data in the page component from your services. You can mock these services out
