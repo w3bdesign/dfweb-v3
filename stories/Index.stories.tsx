4f62d22364621ea44ee0c32338ca6d6147e5a755
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-16 01:24:19 +0100

    Update Index.stories.tsx

--- stories/Index.stories.tsx
+++ stories/Index.stories.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import IndexContent from "../src/components/Index/IndexContent.component";
-import * as HeaderStories from "./Header.stories";
 
 export default {
   title: "Example/Index",
@@ -11,15 +10,4 @@ export default {
 
 const Template: ComponentStory<typeof IndexContent> = () => <IndexContent />;
 
-/*
 export const LoggedIn = Template;
-LoggedIn.args = {
-  // More on composing args: https://storybook.js.org/docs/react/writing-stories/args#args-composition
-  ...HeaderStories.LoggedIn.args
-};
-
-export const LoggedOut = Template;
-LoggedOut.args = {
-  ...HeaderStories.LoggedOut.args
-};
-*/
