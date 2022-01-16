0b544dc45bbb8401bbb58cb3c5327493baea8486
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-16 17:11:30 +0100

    Øk margin top footer

--- src/components/Layout/Footer.component.tsx
+++ src/components/Layout/Footer.component.tsx
@@ -5,7 +5,7 @@ import type { NextComponentType } from "next";
 const Footer: NextComponentType = () => {
   return (
     <footer role="contentinfo" aria-label="Innholdet for bunnteksten med copyright">
-      <div className="mt-4 bg-gray-800 shadow">
+      <div className="mt-10 bg-gray-800 shadow">
         <div
           aria-label="Copyright tekst"
           className="container p-6 mx-auto text-center text-white font-semibold"
