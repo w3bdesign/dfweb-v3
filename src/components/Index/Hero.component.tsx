3963d843bf42784c089635f095f4ee623e0dfce9
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-25 02:47:35 +0100

    Prettier

--- src/components/Index/Hero.component.tsx
+++ src/components/Index/Hero.component.tsx
@@ -31,7 +31,8 @@ const Hero = (): JSX.Element => {
       aria-label="Kontainer for animasjoner av introtekst"
       id="main-hero"
       data-testid="main-hero"
-      className="flex flex-col justify-center text-lg">
+      className="flex flex-col justify-center text-lg"
+    >
       <div ref={boxRef} className="p-4 mt-6 mb-6 bg-white opacity-75">
         <div className="text-black rounded">
           <section role="intro" aria-label="Introduksjonstekst">
