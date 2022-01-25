9576a45d136ec42a469d4f4f39249ea41de5c636
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-25 02:47:16 +0100

    Animer Hero tekst med GSAP

--- src/components/Index/Hero.component.tsx
+++ src/components/Index/Hero.component.tsx
@@ -8,24 +8,22 @@ const Hero = (): JSX.Element => {
 
   // wait until DOM has been rendered
   useEffect(() => {
-    gsap.fromTo(
-      boxRef.current,
-      {
-        scale: 0.2,
-        duration: 2,
-        opacity: 0,
-        ease: "Expo.easeOut",
-        delay: 0.2
-      },
-      {
-        scale: 1,
-        duration: 2,
-        ease: "Expo.easeOut",
-        opacity: 0.75,
-        delay: 0.2
-      }
-    );
-  });
+    gsap.from(boxRef.current, {
+      scale: 0.2,
+      duration: 2,
+      opacity: 0,
+      ease: "Expo.easeOut",
+      delay: 0.2
+    });
+
+    gsap.to(".text-reveal", {
+      clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+      stagger: 0.8,
+      duration: 0.3,
+      delay: 0.2,
+      y: 0
+    });
+  }, []);
 
   return (
     <div
@@ -34,14 +32,14 @@ const Hero = (): JSX.Element => {
       id="main-hero"
       data-testid="main-hero"
       className="flex flex-col justify-center text-lg">
-      <div ref={boxRef} className="p-4 mt-6 mb-6 bg-white opacity-0">
+      <div ref={boxRef} className="p-4 mt-6 mb-6 bg-white opacity-75">
         <div className="text-black rounded">
           <section role="intro" aria-label="Introduksjonstekst">
-            <h1 className="text-5xl text-center p-2">Hei!</h1>
-            <h2 className="text-2xl text-center p-2">
+            <h1 className="text-reveal text-5xl text-center p-2">Hei!</h1>
+            <h2 className="text-reveal text-2xl text-center p-2">
               Jeg heter Daniel Fjeldstad og er en webutvikler.
             </h2>
-            <h2 className="px-6 mt-4 text-lg md:p-0 lg:p-0 xl:p-0 xl:text-center lg:text-left md:text-center xl:text-2xl lg:text-xl md:text-xl md:mx-auto md:w-full lg:w-2/3 xl:w-full">
+            <h2 className="text-reveal px-6 mt-4 text-lg md:p-0 lg:p-0 xl:p-0 xl:text-center lg:text-left md:text-center xl:text-2xl lg:text-xl md:text-xl md:mx-auto md:w-full lg:w-2/3 xl:w-full">
               Jeg kan PHP, mySQL, Wordpress, Javascript, Typescript, React, Vue, Redux, Docker,
               Photoshop og mye mer.
             </h2>
