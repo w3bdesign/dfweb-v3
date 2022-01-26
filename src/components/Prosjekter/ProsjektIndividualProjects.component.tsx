4f48cdf7b47063e6cfedbcf91b8520f1b08e99da
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-26 01:44:24 +0100

    Endre hover bakgrunn

--- src/components/Prosjekter/ProsjektIndividualProjects.component.tsx
+++ src/components/Prosjekter/ProsjektIndividualProjects.component.tsx
@@ -20,7 +20,7 @@ const ProsjektIndividualProjects = ({ projects }: IProject): JSX.Element => {
           <div
             id="projectdiv"
             key={id}
-            className="p-6 text-lg text-black transition duration-500 ease-in-out bg-white rounded shadow hover:bg-gray-200 transform-gpu"
+            className="p-6 text-lg text-black transition duration-500 ease-in-out bg-white rounded shadow  hover:bg-sky-50 transform-gpu"
           >
             <h2 className="text-xl font-black text-center">{name}</h2>
             <div className="mt-6 text-lg text-left lg:text-left md:text-left">
