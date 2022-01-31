2b7eddd9ceebdf3f0c191d576daaece7dc54c1ae
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-31 01:09:50 +0100

    Animasjoner Hero

--- src/components/Index/Hero.component.tsx
+++ src/components/Index/Hero.component.tsx
@@ -12,9 +12,10 @@ const Hero = (): JSX.Element => {
     gsap
       .timeline({ defaults: { opacity: 0, ease: "back" } })
       .from("#main-hero", { autoAlpha: 0 })
-      .from("#main-hero .introtekst", { y: 150, duration: 0.5, stagger: 1 })
-      .from("#main-hero h2", { x: -150, duration: 1, delay: 0.2, stagger: 0.9 })
-      .from("#main-hero svg", { y: 50, duration: 1, stagger: 0.3 });
+      .from("#main-hero .introtekst", { y: -150, duration: 1, stagger: 0.3 })
+      .from("#main-hero .forsteh2", { x: -100, duration: 1 })
+      .from("#main-hero .andreh2", { x: 100, duration: 1, delay: 0.3 })
+      .from("#main-hero svg", { y: 60, duration: 1, stagger: 0.3, ease: "bounce" });
   }, []);
 
   return (
@@ -27,15 +28,16 @@ const Hero = (): JSX.Element => {
       <div className="p-2 mt-10 mb-4 bg-white opacity-75 lg:mt-4 md:mt-4 xl:mt-4">
         <div className="text-black rounded">
           <section role="intro" aria-label="Introduksjonstekst">
-            <span className="introtekst text-5xl text-center p-2">H</span>
-            <span className="introtekst text-5xl text-center p-2">e</span>
-            <span className="introtekst text-5xl text-center p-2">i</span>
-            <span className="introtekst text-5xl text-center p-2">!</span>
-
-            <h2 className="px-6 mt-4 text-lg md:p-0 lg:p-0 xl:p-0 xl:text-center lg:text-left md:text-center xl:text-2xl lg:text-xl md:text-xl md:mx-auto md:w-full lg:w-2/3 xl:w-full">
+            <div className="text-center">
+              <span className="introtekst text-5xl inline-block">H</span>
+              <span className="introtekst text-5xl inline-block">e</span>
+              <span className="introtekst text-5xl inline-block">i</span>
+              <span className="introtekst text-5xl inline-block">!</span>
+            </div>
+            <h2 className="forsteh2 px-6 mt-4 text-lg md:p-0 lg:p-0 xl:p-0 xl:text-center lg:text-left md:text-center xl:text-2xl lg:text-xl md:text-xl md:mx-auto md:w-full lg:w-2/3 xl:w-full">
               Jeg heter Daniel Fjeldstad og er en webutvikler.
             </h2>
-            <h2 className="px-6 mt-4 text-lg md:p-0 lg:p-0 xl:p-0 xl:text-center lg:text-left md:text-center xl:text-2xl lg:text-xl md:text-xl md:mx-auto md:w-full lg:w-2/3 xl:w-full">
+            <h2 className="andreh2 px-6 mt-4 text-lg md:p-0 lg:p-0 xl:p-0 xl:text-center lg:text-left md:text-center xl:text-2xl lg:text-xl md:text-xl md:mx-auto md:w-full lg:w-2/3 xl:w-full">
               Jeg kan PHP, mySQL, Wordpress, Javascript, Typescript, React, Vue, Redux, Docker,
               Photoshop og mye mer.
             </h2>
