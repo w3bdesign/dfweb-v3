829da7101f48f713a44e9a8f8bb7a9232cb43f7c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-01 23:09:58 +0100

    Refaktorering

--- src/components/Index/IndexContent.component.tsx
+++ src/components/Index/IndexContent.component.tsx
@@ -32,33 +32,31 @@ const IndexContent = ({ post }: TData): JSX.Element => (
     <div className="mx-auto mt-16 rounded lg:mt-20 xl:mt-20 bg-graybg shadow-large md:mt-16 sm:mt-64 xs:mt-64">
       <Hero />
       <div className="container grid gap-4 p-4 mx-auto mt-2 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 xs:grid-cols-1">
-        {post &&
-          post.map((content: IContent) => (
-            <section
-              key={content._id}
-              role="contentinfo"
-              aria-label={content.title}
-              data-testid="sanity-section"
-            >
-              <div className="mt-4 p-8 text-lg text-black bg-white rounded shadow min-h-full">
-                <h2 className="text-3xl text-center">{content.title}</h2>
-                <PortableText
-                  className="text-xl"
-                  content={content.text}
-                  serializers={{
-                    code: (props: ISerializerCode) => (
-                      <p className="mt-6 text-lg">{props.children} </p>
-                    ),
-                    link: (props: ISerializerLink) => (
-                      <Link href={props.href}>
-                        <a className="underline">{props.children}</a>
-                      </Link>
-                    )
-                  }}
-                />
-              </div>
-            </section>
-          ))}
+        {post?.map((content: IContent) => (
+          <section
+            key={content._id}
+            role="contentinfo"
+            aria-label={content.title}
+            data-testid="sanity-section">
+            <div className="mt-4 p-8 text-lg text-black bg-white rounded shadow min-h-full">
+              <h2 className="text-3xl text-center">{content.title}</h2>
+              <PortableText
+                className="text-xl"
+                content={content.text}
+                serializers={{
+                  code: (props: ISerializerCode) => (
+                    <p className="mt-6 text-lg">{props.children} </p>
+                  ),
+                  link: (props: ISerializerLink) => (
+                    <Link href={props.href}>
+                      <a className="underline">{props.children}</a>
+                    </Link>
+                  )
+                }}
+              />
+            </div>
+          </section>
+        ))}
       </div>
     </div>
   </main>
