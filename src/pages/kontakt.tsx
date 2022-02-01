73a6c0c2851b1ef9d55e0dbaa9ff40b335b54005
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-01 23:15:35 +0100

    REfaktorer

--- src/pages/kontakt.tsx
+++ src/pages/kontakt.tsx
@@ -3,12 +3,10 @@ import type { NextPage } from "next";
 import KontaktContent from "../components/Kontakt/KontaktContent.component";
 import Layout from "../components/Layout/Layout.component";
 
-const Kontakt: NextPage = () => {
-  return (
-    <Layout title="Kontakt - Portefølje - Dfweb">
-      <KontaktContent />
-    </Layout>
-  );
-};
+const Kontakt: NextPage = () => (
+  <Layout title="Kontakt - Portefølje - Dfweb">
+    <KontaktContent />
+  </Layout>
+);
 
 export default Kontakt;
