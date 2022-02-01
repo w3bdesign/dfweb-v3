2ccf1763828472a0292b9e51a9468c201782d1ae
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-01 03:50:39 +0100

    Forbedre bildelasting

--- src/components/Layout/Navbar.component.tsx
+++ src/components/Layout/Navbar.component.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
-import Image from "next/image";
+//import Image from "next/image";
+
+import Image from "../UI/Image";
 
 import Hamburger from "./Hamburger.component";
 
---
+++ src/components/UI/Image.tsx
@@ -0,0 +1,65 @@
+import NextImage from "next/image";
+import { useEffect, useState } from "react";
+
+/*declare const navigator: Navigator;
+
+interface Navigator {
+    mozConnection?: Navigator["connection"]
+    webkitConnection?: Navigator["connection"]
+}*/
+
+declare const navigator: any;
+
+const Image = (props: any) => {
+  const [loading, setLoading] = useState(props.loading);
+
+  useEffect(() => {
+    // Skip if image is already eager or has priority (disables lazy loading)
+    if (props.loading === "eager" || props.priority) {
+      return;
+    }
+
+    if (!isMobileConnection()) {
+      let clearDefer: { (): void; (): void; };
+      // Set loading to eager if all resources of document are loaded, but defer it a bit
+      const onLoad = () => {
+        clearDefer = defer(() => setLoading("eager"));
+      };
+      window.addEventListener("load", onLoad);
+      return () => {
+        // Clean up the load event listener and an eventual defer
+        window.removeEventListener("load", onLoad);
+        if (clearDefer) {
+          clearDefer();
+        }
+      };
+    }
+  }, [props.loading, props.priority]);
+
+  return <NextImage loading={loading} {...props} />;
+};
+
+const isMobileConnection = () => {
+  
+  const connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+  return (
+    connection?.type === "cellular" ||
+    connection?.effectiveType === "slow-2g" ||
+    connection?.effectiveType === "2g" ||
+    connection?.effectiveType === "3g" ||
+    connection?.saveData === true
+  );
+};
+
+const defer = (callback: IdleRequestCallback) => {
+  // Check if we can use requestIdleCallback
+  if (window.requestIdleCallback) {
+    const handle = window.requestIdleCallback(callback);
+    return () => window.cancelIdleCallback(handle);
+  }
+  // Just defer using setTimeout with some random delay otherwise
+  const handle = setTimeout(callback, 2345 + Math.random() * 1000);
+  return () => clearTimeout(handle);
+};
+
+export default Image;
