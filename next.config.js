c189f68b2b5765c38af6ce3081eade1efaecf43c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-01 03:32:50 +0100

    Forbedre logo lasting

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   reactStrictMode: true,
   poweredByHeader: false,  
   images: {
-    domains: ["cdn.sanity.io"]
+    domains: ["cdn.sanity.io", "v3.dfweb.no", "dfweb.no"]
   }
 };
