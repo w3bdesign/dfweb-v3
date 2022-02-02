eac7c945f1a5544da5780f090b5f334e31df8f03
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-02 23:48:24 +0100

    Få tester til å fungere

--- __tests__/navigation.test.jsx
+++ __tests__/navigation.test.jsx
@@ -9,43 +9,50 @@ import Home from "../src/pages/index";
 describe("Navigation", () => {
   it("Hamburger eksisterer i navigasjon", () => {
     render(<Home />);
-    const Hamburger = screen.getByRole("button", { name: /hamburger/i });
+    const Hamburger = screen.queryByRole("button", { name: /hamburger/i });
     expect(Hamburger).toBeInTheDocument();
   });
 
   it("Hamburger er synlig", () => {
     render(<Home />);
-    const Hamburger = screen.getByRole("button", { name: /hamburger/i });
+    const Hamburger = screen.queryByRole("button", { name: /hamburger/i });
     expect(Hamburger).toBeVisible();
   });
 
   it("Linken Hjem eksisterer i navigasjon", () => {
     render(<Home />);
-    const linkHjem = screen.getByRole("link", { name: /hjem/i });
+    const linkHjem = screen.queryByRole("link", { name: /hjem/i });
     expect(linkHjem).toBeInTheDocument();
   });
 
   it("Linken Prosjekter eksisterer i navigasjon", () => {
     render(<Home />);
-    const linkProsjekter = screen.getByRole("link", { name: /prosjekter/i });
+    const linkProsjekter = screen.queryByRole("link", { name: /prosjekter/i });
     expect(linkProsjekter).toBeInTheDocument();
   });
 
   it("Linken CV eksisterer i navigasjon", () => {
     render(<Home />);
-    const linkCV = screen.getByRole("link", { name: /cv/i });
+    const linkCV = screen.queryByRole("link", { name: /cv/i });
     expect(linkCV).toBeInTheDocument();
   });
 
   it("Linken Github eksisterer i navigasjon", () => {
     render(<Home />);
-    const linkGithub = screen.getByRole("link", { name: /github/i });
+    const linkGithub = screen.queryByRole("link", { name: /github/i });
     expect(linkGithub).toBeInTheDocument();
   });
 
   it("Linken Kontakt eksisterer i navigasjon", () => {
     render(<Home />);
-    const linkKontakt = screen.getByRole("link", { name: /kontakt/i });
+    const linkKontakt = screen.queryByRole("link", { name: /kontakt/i });
     expect(linkKontakt).toBeInTheDocument();
+  });
+
+  it("Logo eksisterer i navigasjon", () => {
+    render(<Home />);
+    //const logo = screen.getByRole("img", { name: /dfweb logo/i });
+    const logo = screen.queryByRole("img", { name: /dfweb logo/i });
+    expect(logo).toBeInTheDocument();
   });
 });
