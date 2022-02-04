7545ffa7471de243b7666eda23f2eff295832158
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-04 23:58:44 +0100

    Test Codeclimate

--- src/components/Index/Hero.component.tsx
+++ src/components/Index/Hero.component.tsx
@@ -23,26 +23,7 @@ const Hero = (): JSX.Element => {
       id="main-hero"
       data-testid="main-hero"
       className="flex flex-col justify-center text-lg invisible">
-      <div className="p-2 mt-10 mb-4 bg-white opacity-75 lg:mt-4 md:mt-4 xl:mt-4">
-        <div className="text-black rounded">
-          <section role="intro" aria-label="Introduksjonstekst">
-            <div className="text-center">
-              <span className="introtekst text-5xl inline-block invisible">H</span>
-              <span className="introtekst text-5xl inline-block invisible ">e</span>
-              <span className="introtekst text-5xl inline-block invisible">i</span>
-              <span className="introtekst text-5xl inline-block invisible">!</span>
-            </div>
-            <h2 className="forsteh2 invisible px-6 mt-4 text-lg md:p-0 lg:p-0 xl:p-0 xl:text-center lg:text-left md:text-center xl:text-2xl lg:text-xl md:text-xl md:mx-auto lg:w-2/3">
-              Jeg heter Daniel Fjeldstad og er en webutvikler.
-            </h2>
-            <h2 className="andreh2 invisible px-6 mt-4 text-lg md:p-0 lg:p-0 xl:p-0 xl:text-center lg:text-left md:text-center xl:text-2xl lg:text-xl md:text-xl md:mx-auto lg:w-2/3">
-              Jeg kan PHP, Wordpress, Javascript, Typescript, React, Vue, Docker, Photoshop og mye
-              mer.
-            </h2>
-            <Icons />
-          </section>
-        </div>
-      </div>
+Test forenkling
     </div>
   );
 };
