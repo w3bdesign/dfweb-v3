5bd0e033f7968cc28a4414462275c6fc6df84df6
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-04 02:13:48 +0100

    Fiks Codacy problemer

--- src/components/Layout/Navbar.component.tsx
+++ src/components/Layout/Navbar.component.tsx
@@ -17,19 +17,22 @@ const Navbar = (): JSX.Element => (
       <div
         id="main-navigation"
         data-cy="main-navigation"
-        className="container flex items-center mx-auto md:flex-wrap lg:flex-wrap xl:flex-wrap">
+        className="container flex items-center mx-auto md:flex-wrap lg:flex-wrap xl:flex-wrap"
+      >
         <div className="flex w-full font-extrabold text-white md:w-1/2 md:justify-start">
           <Image src="/logo.svg" alt="Dfweb Logo" width={150} height={45} />
         </div>
         <div
           id="hamburger-div"
           data-cy="hamburger-div"
-          className="flex content-center justify-between md:w-1/2 md:justify-end p-3">
+          className="flex content-center justify-between md:w-1/2 md:justify-end p-3"
+        >
           <Hamburger />
           <ul
             role="navigation"
             aria-label="Navigasjon"
-            className="items-center justify-between flex-1 hidden list-reset md:flex lg:flex xl:flex lg:-mr-4 xl:-mr-4">
+            className="items-center justify-between flex-1 hidden list-reset md:flex lg:flex xl:flex lg:-mr-4 xl:-mr-4"
+          >
             {LINKS?.map((link) => (
               <li key={link.id} className="link mr-3">
                 {link.external ? (
@@ -38,7 +41,8 @@ const Navbar = (): JSX.Element => (
                       rel="noopener noreferrer"
                       aria-label={link.text}
                       target="_blank"
-                      className="navbar-link inline-block text-xl text-white ">
+                      className="navbar-link inline-block text-xl text-white "
+                    >
                       {link.text}
                     </a>
                   </Link>
@@ -46,7 +50,8 @@ const Navbar = (): JSX.Element => (
                   <Link href={link.url}>
                     <a
                       aria-label={link.text}
-                      className="navbar-link eds-top-navigation-item  inline-block text-xl text-white ">
+                      className="navbar-link eds-top-navigation-item  inline-block text-xl text-white "
+                    >
                       {link.text}
                     </a>
                   </Link>
