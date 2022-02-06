9d0e4aa79dec88890307bb4316eabb89e5856fbb
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-06 02:16:19 +0100

    Fjern ubrukt import

--- __tests__/image.test.jsx
+++ __tests__/image.test.jsx
@@ -3,14 +3,15 @@
  */
 
 import React from "react";
-import { render, screen, prettyDOM } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Image from "../src/components/UI/Image.component";
+// Debug: //console.log(prettyDOM(JestImage));
 
 describe("Mobile menu", () => {
   it("Expect Image component toBeInTheDocument", () => {
     render(<Image alt="Jest" src="/blue-hero.jpg" width="100" height="100" />);
     const JestImage = screen.getByAltText(/jest/i);
-    //console.log(prettyDOM(JestImage));
+    
     expect(JestImage).toBeInTheDocument();
   });
 });
