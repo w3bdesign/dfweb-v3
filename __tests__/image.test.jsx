7f33c15f2ee83fded803c538ec96d630b6290db4
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-06 02:37:10 +0100

    Test Jest

--- __tests__/image.test.jsx
+++ __tests__/image.test.jsx
@@ -3,6 +3,7 @@
  */
 
 import React from "react";
+import { useEffect } from 'react';
 import { render, screen } from "@testing-library/react";
 import Image from "../src/components/UI/Image.component";
 // Debug: //console.log(prettyDOM(JestImage));
@@ -11,7 +12,20 @@ describe("Mobile menu", () => {
   it("Expect Image component toBeInTheDocument", () => {
     render(<Image alt="Jest" src="/blue-hero.jpg" width="100" height="100" />);
     const JestImage = screen.getByAltText(/jest/i);
-    
     expect(JestImage).toBeInTheDocument();
+  });
+
+  it("Expect mock", () => {
+    const isMobileConnection = jest.fn();
+    //useEffect.mock.calls[0]();
+    jest.mock('React', () => ({
+        ...jest.requireActual('React'),
+        useEffect: jest.fn(),
+      }));
+
+    render(<Image alt="Jest" src="/blue-hero.jpg" width="100" height="100" />);
+
+    //expect(isMobileConnection.mock.calls.length).toBe(1);
+    expect(isMobileConnection.mock.calls.length).toBe(0);
   });
 });
