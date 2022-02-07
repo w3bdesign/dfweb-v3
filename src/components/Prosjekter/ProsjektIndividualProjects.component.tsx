e8892f04f57e024e8212ea190154f118e5a55059
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-07 18:31:41 +0100

    Prettier

--- src/components/Prosjekter/ProsjektIndividualProjects.component.tsx
+++ src/components/Prosjekter/ProsjektIndividualProjects.component.tsx
@@ -28,10 +28,7 @@ const ProsjektIndividualProjects = ({ projects }: IProject): JSX.Element => {
     <>
       {projects.map(
         ({ id, name, description, subdescription, urlwww, urlgithub, projectimage }) => (
-          <div
-            id="projectdiv"
-            key={id}
-            className="p-6 text-lg text-black  bg-white rounded shadow">
+          <div id="projectdiv" key={id} className="p-6 text-lg text-black  bg-white rounded shadow">
             <h2 className="text-xl font-black text-center">{name}</h2>
             <div className="mt-6 text-lg text-left lg:text-left md:text-left">
               <p>{description}</p>
