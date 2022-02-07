08814cf265eb7e4f15ce892c0edbe6ba380f8cb8
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-07 19:17:53 +0100

    Deepsource

--- src/lib/sanity.server.ts
+++ src/lib/sanity.server.ts
@@ -14,6 +14,6 @@ export const previewClient = createClient({
 });
 
 // Helper function for easily switching between normal client and preview client
-// export const getClient = (usePreview: SanityClient) => (usePreview ? previewClient : sanityClient);
 
-export const getClient = (usePreview: any) => (usePreview ? previewClient : sanityClient);
+export const getClient = (usePreview: typeof sanityClient) =>
+  usePreview ? previewClient : sanityClient;
