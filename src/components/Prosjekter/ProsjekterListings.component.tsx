307fbee49460492a612e05a80a452e7851ec7a2f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-07 20:48:00 +0100

    Update ProsjekterListings.component.tsx

--- src/components/Prosjekter/ProsjekterListings.component.tsx
+++ src/components/Prosjekter/ProsjekterListings.component.tsx
@@ -59,8 +59,8 @@ const ProsjekterListings = ({ projects, categories }: IProjectCategory): JSX.Ele
                 <option label="" value="">
                   Ingen filtrering
                 </option>
-                {categories?.map((category: string, id: number) => (
-                  <option key={id} value={category}>
+                {categories?.map((category: string) => (
+                  <option key={category} value={category}>
                     {category}
                   </option>
                 ))}
