68602696593e4a3fb52f7c3805b3d48a700e57f8
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-08 04:00:35 +0100

    Update Image.stories.tsx

--- stories/components/Image.stories.tsx
+++ stories/components/Image.stories.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { ImageProps } from "next/image";
-
 import Image from "../../src/components/UI/Image.component";
-
-
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
