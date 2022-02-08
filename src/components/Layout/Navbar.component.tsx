680cf7cc66a5d4d4b0af8cbafade4b43bf1cef65
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-08 02:12:01 +0100

    Update Navbar.component.tsx

--- src/components/Layout/Navbar.component.tsx
+++ src/components/Layout/Navbar.component.tsx
@@ -27,6 +27,35 @@ const Navbar = (): JSX.Element => (
           data-cy="hamburger-div"
           className="flex content-center justify-between md:w-1/2 md:justify-end p-3">
           <Hamburger />
+          <ul
+            role="navigation"
+            aria-label="Navigasjon"
+            className="items-center justify-between flex-1 hidden list-reset md:flex lg:flex xl:flex lg:-mr-4 xl:-mr-4">
+            {LINKS?.map((link) => (
+              <li key={link.id} className="link mr-3">
+                {link.external && (
+                  <Link href={link.url} passHref>
+                    <a
+                      rel="noopener noreferrer"
+                      aria-label={link.text}
+                      target="_blank"
+                      className="navbar-link inline-block text-xl text-white">
+                      {link.text}
+                    </a>
+                  </Link>
+                )}
+                {!link.external && (
+                  <Link href={link.url} passHref>
+                    <a
+                      aria-label={link.text}
+                      className="navbar-link eds-top-navigation-item  inline-block text-xl text-white">
+                      {link.text}
+                    </a>
+                  </Link>
+                )}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </nav>
