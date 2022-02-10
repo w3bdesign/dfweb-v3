a94cba677d5540e5688534483d11dc3814e20747
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-10 23:10:20 +0100

    Legg til css klasse prop

--- src/animations/functions.tsx
+++ src/animations/functions.tsx
@@ -3,27 +3,29 @@ import { ReactNode } from "react";
 
 import { bounceVariants, scaleInVariant, scaleInItemVariant } from "./variants";
 
-interface IBounceProps {
-  children: ReactNode;
-}
-
-interface IScaleInProps {
+interface IAnimateProps {
   children: ReactNode;
   cssClass?: string;
 }
 
-export const BounceInWhenVisible = ({ children }: IBounceProps) => (
-  <motion.div initial="offscreen" whileInView="onscreen" viewport={{ once: true, amount: 0.8 }}>
+export const BounceInWhenVisible = ({ children, cssClass }: IAnimateProps) => (
+  <motion.div
+    initial="offscreen"
+    whileInView="onscreen"
+    viewport={{ once: true, amount: 0.8 }}
+    className={cssClass}>
     <motion.div variants={bounceVariants}>{children}</motion.div>
   </motion.div>
 );
 
-export const ScaleIn = ({ children, cssClass }: IScaleInProps) => (
+export const ScaleIn = ({ children, cssClass }: IAnimateProps) => (
   <motion.span initial="hidden" animate="visible" variants={scaleInVariant} className={cssClass}>
     {children}
   </motion.span>
 );
 
-export const ScaleInItem = ({ children }: IBounceProps) => (
-  <motion.span variants={scaleInItemVariant}>{children}</motion.span>
+export const ScaleInItem = ({ children, cssClass }: IAnimateProps) => (
+  <motion.span variants={scaleInItemVariant} className={cssClass}>
+    {children}
+  </motion.span>
 );
