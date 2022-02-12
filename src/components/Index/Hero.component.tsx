8b90eed7baa77cc9214fc34638485462e39747a8
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-12 06:56:44 +0100

    Refresh

--- src/components/Index/Hero.component.tsx
+++ src/components/Index/Hero.component.tsx
@@ -15,7 +15,8 @@ const Hero = (): JSX.Element => (
     aria-label="Kontainer for animasjoner av introtekst"
     id="main-hero"
     data-testid="main-hero"
-    className="flex flex-col justify-center text-lg ">
+    className="flex flex-col justify-center text-lg "
+  >
     <div className="mt-10 mb-4 bg-white p-2 opacity-75 md:mt-4 lg:mt-4 xl:mt-4">
       <div className="rounded text-black">
         <section role="heading" aria-label="Introduksjonstekst" aria-level={2}>
@@ -24,12 +25,14 @@ const Hero = (): JSX.Element => (
           </FadeDown>
           <FadeUp
             delay={1.7}
-            cssClass="mt-4 px-6 text-lg md:mx-auto md:p-0 md:text-center md:text-xl lg:w-2/3 lg:p-0 lg:text-left lg:text-xl xl:p-0 xl:text-center xl:text-2xl">
+            cssClass="mt-4 px-6 text-lg md:mx-auto md:p-0 md:text-center md:text-xl lg:w-2/3 lg:p-0 lg:text-left lg:text-xl xl:p-0 xl:text-center xl:text-2xl"
+          >
             <h2>Jeg heter Daniel Fjeldstad og er en webutvikler.</h2>
           </FadeUp>
           <FadeDown
             delay={2.2}
-            cssClass="mt-4 px-6 text-lg md:mx-auto md:p-0 md:text-center md:text-xl lg:w-2/3 lg:p-0 lg:text-left lg:text-xl xl:p-0 xl:text-center xl:text-2xl">
+            cssClass="mt-4 px-6 text-lg md:mx-auto md:p-0 md:text-center md:text-xl lg:w-2/3 lg:p-0 lg:text-left lg:text-xl xl:p-0 xl:text-center xl:text-2xl"
+          >
             <h2>
               Jeg kan PHP, Wordpress, Javascript, Typescript, React, Vue, Docker, Photoshop og mye
               mer.
