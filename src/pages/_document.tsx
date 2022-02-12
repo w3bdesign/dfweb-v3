92d6e1bc1594f83b2c63e826bbd4aca8441eab64
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-12 23:34:33 +0100

    Forbedre CSP

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,12 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
-import generateCSP from "../utils/generate-csp";
 
+import crypto from "crypto";
+
+const cspHashOf = (text: crypto.BinaryLike) => {
+  const hash = crypto.createHash("sha256");
+  hash.update(text);
+  return `'sha256-${hash.digest("base64")}'`;
+};
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -8,9 +14,18 @@ class MyDocument extends Document {
   }
 
   render(): JSX.Element {
+    const nonce = crypto.randomBytes(8).toString("base64");
+    let csp = `default-src 'self'; script-src 'self' ${cspHashOf(
+      NextScript.getInlineScriptSource(this.props)
+    )}`;
+    if (process.env.NODE_ENV !== "production") {
+      csp = `style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com data:; default-src 'self'; script-src 'unsafe-eval' 'self' ${cspHashOf(
+        NextScript.getInlineScriptSource(this.props)
+      )}`;
+    }
     return (
       <Html lang="nb">
-        <Head>
+        <Head nonce={nonce}>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
@@ -21,7 +36,7 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" href="/logo.png" />
 
-          <meta httpEquiv="Content-Security-Policy" content={generateCSP()} />
+          <meta httpEquiv="Content-Security-Policy" content={csp} />
 
           <meta property="og:title" content="Daniel Fjeldstad" />
           <meta name="author" content="Daniel Fjeldstad" />
