4047c758ccc2913cecc1a083f9de7af01c9482f8
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-12 06:59:35 +0100

    Fjern form

--- src/components/Prosjekter/ProsjekterListings.component.tsx
+++ src/components/Prosjekter/ProsjekterListings.component.tsx
@@ -53,30 +53,26 @@ const ProsjekterListings = ({ projects, categories }: IProjectCategory): JSX.Ele
       <div className="container mx-auto rounded">
         <div className="px-4 mx-auto mt-4 lg:px-0 xl:px-0 md:px-0">
           <span className="flex justify-end mb-4">
-            <form>
-              <label
-                className="p-2 mr-4 text-lg"
-                aria-label="kategorifilter"
-                htmlFor="kategorifilter"
-              >
-                Filtrer kategori:
-              </label>
-              <select
-                id="kategorifilter"
-                name="kategorifilter"
-                onChange={handleFilterChange}
-                className="w-40 p-2 leading-tight text-black border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-              >
-                <option label="" value="">
-                  Ingen filtrering
+            <label
+              className="p-2 mr-4 text-lg"
+              aria-label="kategorifilter"
+              htmlFor="kategorifilter">
+              Filtrer kategori:
+            </label>
+            <select
+              id="kategorifilter"
+              name="kategorifilter"
+              onChange={handleFilterChange}
+              className="w-40 p-2 leading-tight text-black border rounded shadow appearance-none focus:outline-none focus:shadow-outline">
+              <option label="" value="">
+                Ingen filtrering
+              </option>
+              {categories?.map((category: string) => (
+                <option key={category} value={category}>
+                  {category}
                 </option>
-                {categories?.map((category: string) => (
-                  <option key={category} value={category}>
-                    {category}
-                  </option>
-                ))}
-              </select>
-            </form>
+              ))}
+            </select>
           </span>
           {prosjekt && <ProsjekterSingleProject projects={prosjekt} />}
         </div>
