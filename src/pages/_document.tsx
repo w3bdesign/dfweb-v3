36518bab9cf857956db4e667119312e1fbcd77bb
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-12 23:49:48 +0100

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,7 +23,7 @@ class MyDocument extends Document {
 
     let csp = `style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com data:; default-src 'self'; script-src 'unsafe-eval' 'self' ${cspHashOf(
         NextScript.getInlineScriptSource(this.props)
-      )} `;
+      )} form-action 'self'`;
 
 
 
