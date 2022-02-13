6ec059a86d575a8839e1e4fc24c062e27b259cc5
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-13 03:17:09 +0100

    Forbedre csp

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+import { headers } from "./src/utils/headers";
+
 module.exports = {
   reactStrictMode: true,
   poweredByHeader: false,
   images: {
     domains: ["cdn.sanity.io", "v3.dfweb.no", "dfweb.no"],
     minimumCacheTTL: 600
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers
+      }
+    ];
   }
 };
---
+++ src/utils/headers.ts
@@ -0,0 +1,34 @@
+export const headers = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on"
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload"
+  },
+  {
+    key: "Server",
+    value: "Apache" // fake server value
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff"
+  },
+  {
+    key: "X-Frame-Options",
+    value: "sameorigin"
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block"
+  },
+  {
+    key: "Referrer-Policy",
+    value: "same-origin"
+  },
+  {
+    key: "Permissions-Policy",
+    value: "geolocation=*" // allow specified policies here
+  }
+];
