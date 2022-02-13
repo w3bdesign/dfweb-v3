5e5ec88c2ed9e64df0f2721856c6840699be4348
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-13 03:23:33 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,40 @@
 /** @type {import('next').NextConfig} */
 
-import { headers } from "./src/utils/headers";
+//import { headers } from "./src/utils/headers";
+const headers = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on"
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload"
+  },
+  {
+    key: "Server",
+    value: "Apache" // fake server value
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff"
+  },
+  {
+    key: "X-Frame-Options",
+    value: "sameorigin"
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block"
+  },
+  {
+    key: "Referrer-Policy",
+    value: "same-origin"
+  },
+  {
+    key: "Permissions-Policy",
+    value: "geolocation=*" // allow specified policies here
+  }
+];
 
 module.exports = {
   reactStrictMode: true,
