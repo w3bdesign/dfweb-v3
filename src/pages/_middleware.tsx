f3782addc752a1265d680f8cf804bad827f31f38
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-14 18:08:27 +0100

    Fiks middleware

--- src/pages/_middleware.tsx
+++ src/pages/_middleware.tsx
@@ -3,25 +3,20 @@ import { NextResponse } from "next/server";
 
 export function middleware(_req: NextRequest, _ev: NextFetchEvent) {
   const ContentSecurityPolicy = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com cdn.usefathom.com;
-    child-src *.youtube.com *.google.com *.twitter.com;
-    style-src 'self' 'unsafe-inline' *.googleapis.com;
+    default-src 'self' v3.dfweb.no dfweb.no;
+    script-src 'self' v3.dfweb.no dfweb.no;
+    child-src *.google.com ;
+    style-src 'self';
     img-src * blob: data:;
     media-src 'none';
-    connect-src *;
+    connect-src api.emailjs.com;
     font-src 'self' fonts.gstatic.com;
   `;
 
   const response = NextResponse.next();
 
   response.headers.set("Content-Security-Policy", ContentSecurityPolicy.replace(/\n/g, ""));
-  response.headers.set("Referrer-Policy", "origin-when-cross-origin");
-  response.headers.set("Permissions-Policy", "camera=(), microphone=(), geolocation=()");
-  response.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
-  response.headers.set("X-Frame-Options", "DENY");
-  response.headers.set("X-Content-Type-Options", "nosniff");
-  response.headers.set("X-DNS-Prefetch-Control", "on");
+
 
   return response;
 }
