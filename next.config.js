ceeb816dbd4205f1e0b27d743e2d9b4c33ccf9ac
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-14 04:28:56 +0100

    Test csp

--- next.config.js
+++ next.config.js
@@ -10,12 +10,17 @@ const generateCsp = () => {
   hash.update(nanoid());
   const production = process.env.NODE_ENV === "production";
 
-  return `default-src 'self'; style-src https://fonts.googleapis.com 'self' 'sha256-${hash.digest(
+  /*return `default-src 'self'; style-src https://fonts.googleapis.com 'self' 'sha256-${hash.digest(
     "base64"
   )}'; script-src 'sha256-${hashsecond.digest("base64")}' 'self' ${
     production ? "" : "'unsafe-eval'"
   }; font-src https://fonts.gstatic.com 'self' data:; img-src https://lh3.googleusercontent.com https://res.cloudinary.com https://s.gravatar.com 'self' data:;`;
-};
+};*/
+
+
+return `frame-ancestors 'self';block-all-mixed-content;default-src 'self';script-src 'self' 'sha256-${secondhash.digest("base64")}' 'report-sample' v3.dfweb.no dfweb.no;style-src 'self' 'sha256-${hash.digest("base64")}'; dfweb.no v3.dfweb.no fonts.googleapis.com;object-src 'none';frame-src 'self';child-src 'self';img-src 'self' data: fonts.gstatic.com;font-src 'self' fonts.googleapis.com fonts.gstatic.com;connect-src 'self' api.emailjs.com emailjs.com fonts.googleapis.com fonts.gstatic.com;manifest-src 'self';base-uri 'self';form-action 'self';media-src 'self';worker-src 'self';`
+
+
 
 const csp = `frame-ancestors 'self';block-all-mixed-content;default-src 'self';script-src 'self' 'report-sample' v3.dfweb.no dfweb.no;style-src 'self' dfweb.no v3.dfweb.no fonts.googleapis.com;object-src 'none';frame-src 'self';child-src 'self';img-src 'self' data: fonts.gstatic.com;font-src 'self' fonts.googleapis.com fonts.gstatic.com;connect-src 'self' api.emailjs.com emailjs.com fonts.googleapis.com fonts.gstatic.com;manifest-src 'self';base-uri 'self';form-action 'self';media-src 'self';worker-src 'self';`;
 
