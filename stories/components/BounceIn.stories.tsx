8a5ed4fad937b50f4c522078c993fdaec2d4c710
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-15 22:43:15 +0100

    Fullfør BounceIn story

--- stories/components/BounceIn.stories.tsx
+++ stories/components/BounceIn.stories.tsx
@@ -1,17 +1,36 @@
 import React from "react";
+
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import BounceIn from "../../src/components/Animations/BounceIn.component";
 
+import { IAnimateProps } from "../../src/components/Animations/types/Animations.types";
+
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Components/Animations/BounceIn",
-  component: BounceIn
+  component: BounceIn,
+  argTypes: {
+    children: {
+      description: "Children content to be rendered",
+      options: ["BounceIn animation", "Sample animation text"],
+      mapping: {
+        BounceIn: <h1>BounceIn animation</h1>,
+        Sample: <h1>Sample animation text</h1>
+      }
+    },
+    cssClass: { description: "CSS class to append to content" }
+  }
 } as ComponentMeta<typeof BounceIn>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof BounceIn> = () => <BounceIn>BounceIn animation</BounceIn>;
+const Template: ComponentStory<typeof BounceIn> = ({ children, cssClass }: IAnimateProps) => (
+  <BounceIn cssClass={cssClass}>{children}</BounceIn>
+);
 
 export const Primary = Template;
 
-Primary.args = {};
+Primary.args = {
+  children: <h1>BounceIn animation</h1>,
+  cssClass: "text-center"
+};
