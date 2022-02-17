8b20dc0b8373849d63e85b1f2075e39e5e346f52
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-18 00:56:55 +0100

    Npm refresh

---
+++ stories/components/Icons.stories.tsx
@@ -0,0 +1,42 @@
+import React, { ReactNode, MouseEventHandler } from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Button from "../../src/components/UI/Button.component";
+
+interface IButtonProps {
+  text?: string;
+  children?: ReactNode | JSX.Element;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Components/UI/Button",
+  component: Button,
+  argTypes: {
+    text: { description: "Text for button" },
+    children: {
+      description: "Children content to be rendered",
+      options: ["Button", "Second Button"],
+      mapping: {
+        Button: <h1>Button</h1>,
+        SecondButton: <h1>Second Button</h1>
+      }
+    },
+    onClick: { action: "onClick" }
+  }
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = ({ text, children, onClick }: IButtonProps) => (
+  <Button text={text} onClick={onClick}>
+    {children}
+  </Button>
+);
+
+export const Primary = Template;
+
+Primary.args = {
+  text: "",
+  children: <h1>Button</h1>
+};
