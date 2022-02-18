b80fac8cb671d86e2917dcd69a2a9841c0aa869c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-18 01:01:05 +0100

    Icons story

--- stories/components/Icons.stories.tsx
+++ stories/components/Icons.stories.tsx
@@ -1,42 +1,16 @@
-import React, { ReactNode, MouseEventHandler } from "react";
+import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import Button from "../../src/components/UI/Button.component";
-
-interface IButtonProps {
-  text?: string;
-  children?: ReactNode | JSX.Element;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
-}
+import Icons from "../../src/components/Index/Icons.component";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Components/UI/Button",
-  component: Button,
-  argTypes: {
-    text: { description: "Text for button" },
-    children: {
-      description: "Children content to be rendered",
-      options: ["Button", "Second Button"],
-      mapping: {
-        Button: <h1>Button</h1>,
-        SecondButton: <h1>Second Button</h1>
-      }
-    },
-    onClick: { action: "onClick" }
-  }
-} as ComponentMeta<typeof Button>;
+  title: "Components/Index/Icons",
+  component: Icons
+} as ComponentMeta<typeof Icons>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = ({ text, children, onClick }: IButtonProps) => (
-  <Button text={text} onClick={onClick}>
-    {children}
-  </Button>
-);
+const Template: ComponentStory<typeof Icons> = () => <Icons />;
 
-export const Primary = Template;
+export const Icon = Template;
 
-Primary.args = {
-  text: "",
-  children: <h1>Button</h1>
-};
