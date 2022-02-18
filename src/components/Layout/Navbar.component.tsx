d80fe4d5dd9f61e81b775ce5672c57c97af1e640
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-18 23:08:17 +0100

    Padding mobil logo

--- src/components/Layout/Navbar.component.tsx
+++ src/components/Layout/Navbar.component.tsx
@@ -26,7 +26,7 @@ const Navbar = (): JSX.Element => {
           data-cy="main-navigation"
           className="container flex items-center mx-auto md:flex-wrap lg:flex-wrap xl:flex-wrap">
           <div className="flex w-full text-white md:w-1/2 md:justify-start">
-            <img src="/logo.svg" alt="Dfweb Logo" width="150" height="45" />
+            <img className="p-2 lg:p-0" src="/logo.svg" alt="Dfweb Logo" width="150" height="45" />
           </div>
           <div
             id="hamburger-div"
