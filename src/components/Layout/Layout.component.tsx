f687b875138b91dda5610a5974a9620ec3146a77
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-19 03:56:06 +0100

    Typescript

--- src/components/Layout/Layout.component.tsx
+++ src/components/Layout/Layout.component.tsx
@@ -3,16 +3,18 @@ import { ReactNode } from "react";
 import Footer from "./Footer.component";
 import Header from "./Header.component";
 
+type TTitle = "Forside" | "Kontakt" | "CV" | "Prosjekter";
+
 interface ILayoutProps {
   children?: ReactNode;
-  title: string;
+  title: TTitle;
 }
 
 /**
  * Renders layout for each page. Also passes along the title to the Header component.
  * @function Layout
  * @param {ReactNode} children - Children to be rendered by Layout component
- * @param {string} title - Title for the page. Is set in <title>{title}</title>
+ * @param {TTitle} title - Title for the page. Is set in <title>{title}</title>
  * @returns {JSX.Element} - Rendered component
  */
 
