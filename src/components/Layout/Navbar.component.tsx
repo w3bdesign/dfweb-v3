b515f84ee804d42b0ca388a642d6df6325adc6e2
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-19 04:16:12 +0100

    Forbedre accessibility

--- src/components/Layout/Navbar.component.tsx
+++ src/components/Layout/Navbar.component.tsx
@@ -24,22 +24,18 @@ const Navbar = (): JSX.Element => {
         <div
           id="main-navigation"
           data-cy="main-navigation"
-          className="container flex items-center mx-auto md:flex-wrap lg:flex-wrap xl:flex-wrap"
-        >
+          className="container flex items-center mx-auto md:flex-wrap lg:flex-wrap xl:flex-wrap">
           <div className="flex w-full text-white md:w-1/2 md:justify-start">
             <img className="p-2 lg:p-0" src="/logo.svg" alt="Dfweb Logo" width="150" height="45" />
           </div>
           <div
             id="hamburger-div"
             data-cy="hamburger-div"
-            className="flex content-center justify-between md:w-1/2 md:justify-end p-3"
-          >
+            className="flex content-center justify-between md:w-1/2 md:justify-end p-3">
             <Hamburger />
             <ul
-              role="navigation"
               aria-label="Navigasjon"
-              className="items-center justify-between flex-1 hidden list-reset md:flex lg:flex xl:flex lg:-mr-4 xl:-mr-4"
-            >
+              className="items-center justify-between flex-1 hidden list-reset md:flex lg:flex xl:flex lg:-mr-4 xl:-mr-4">
               {LINKS?.map((link) => (
                 <li key={link.id} className="link mr-3">
                   {link.external ? (
@@ -48,8 +44,7 @@ const Navbar = (): JSX.Element => {
                         rel="noopener noreferrer"
                         aria-label={link.text}
                         target="_blank"
-                        className="navbar-link inline-block text-xl text-white"
-                      >
+                        className="navbar-link inline-block text-xl text-white">
                         {link.text}
                       </a>
                     </Link>
@@ -60,8 +55,7 @@ const Navbar = (): JSX.Element => {
                         className={`navbar-link eds-top-navigation-item inline-block text-xl text-white
                       ${activeLink(link.url, router.pathname)}
 
-                      `}
-                      >
+                      `}>
                         {link.text}
                       </a>
                     </Link>
