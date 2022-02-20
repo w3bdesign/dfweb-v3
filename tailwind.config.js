a30100d390ca0ee68618be7a3d5de1e8a9849e38
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-20 07:03:01 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {      height: {        '128': '31.25rem',      }    }
+    extend: { height: { 128: "31.25rem" } }
   },
   variants: {
     extend: {}
