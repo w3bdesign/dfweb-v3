3304764fd7ca6575e65c8e4cfcf4dce083ff9319
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-20 06:55:09 +0100

    Custom height for ipad

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {      height: {        '128': '31.25rem',      }    }
   },
   variants: {
     extend: {}
