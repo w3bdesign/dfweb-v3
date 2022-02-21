1f0ce2b6e6eb52f5da476fb05dd7d6cec36a983c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-21 23:52:02 +0100

    Test Chromatic

--- stories/components/Button.stories.tsx
+++ stories/components/Button.stories.tsx
@@ -27,7 +27,7 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = ({ text, children, onClick }: IButtonProps) => (
   <Button text={text} onClick={onClick}>
-    TEST CHRomatic
+    {children}
   </Button>
 );
 
