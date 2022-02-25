1e0b229555434d4ba4e5a4a19f890bf724f7ebbb
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-25 01:03:17 +0100

    Flere tester

---
+++ __tests__/FadeDown.test.jsx
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import FadeDown from "../src/components/Animations/FadeDown.component";
+import { render } from "@testing-library/react";
+
+describe("FadeDown", () => {
+  it("FadeDown laster inn og kan vises", () => {
+    const { container } = render(<FadeDown delay={1}>FadeDown</FadeDown>);
+    expect(container).toMatchSnapshot();
+  });
+});
---
+++ __tests__/FadeUp.test.jsx
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import FadeUp from "../src/components/Animations/FadeUp.component";
+import { render } from "@testing-library/react";
+
+describe("FadeUp", () => {
+  it("FadeUp laster inn og kan vises", () => {
+    const { container } = render(<FadeUp delay={1}>FadeUp</FadeUp>);
+    expect(container).toMatchSnapshot();
+  });
+});
