108e82692dacabd6d349f2b5072addc29b39ade9
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-25 01:25:12 +0100

    handleOnClick for Button test

--- __tests__/UI/Button.test.jsx
+++ __tests__/UI/Button.test.jsx
@@ -5,9 +5,11 @@
 import Button from "../../src/components/UI/Button.component";
 import { render } from "@testing-library/react";
 
+const handleOnClick = jest.fn();
+
 describe("Button", () => {
   it("Button laster inn og kan vises", () => {
-    const { container } = render(<Button onClick={jest.fn}>Button</Button>);
+    const { container } = render(<Button onClick={handleOnClick}>Button</Button>);
     expect(container).toMatchSnapshot();
   });
 });
