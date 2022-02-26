23c7824338e1dd40bdf17f18ab59bd7bba16669e
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-26 19:02:02 +0100

    Sett opp New Relic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,6 +38,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script async type="text/javascript" src="/js/newrelic.js" />
         </body>
       </Html>
     );
