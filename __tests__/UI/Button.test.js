629565d70997edcbca2bd3c55875a7a8f0e18657
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-26 21:00:08 +0100

    Fiks Button test

--- __tests__/UI/Button.test.js
+++ __tests__/UI/Button.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import Button from "../../src/components/UI/Button.component";
 
@@ -11,9 +11,16 @@ const handleOnClick = jest.fn();
 describe("Button", () => {
   it("Button laster inn og kan vises", () => {
     render(<Button onClick={handleOnClick}>Button</Button>);
-    const container = screen.queryByRole("button", {
+    const button = screen.queryByRole("button", {
       name: /button/i
     });
-    expect(container).toBeInTheDocument();
+
+    console.log("Button ....")
+    screen.debug();
+    console.log(button)
+
+
+
+    expect(button).toBeInTheDocument();
   });
 });
