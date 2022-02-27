d0286a2650fa17ca8f74117824f3b20c563b814b
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-27 02:17:41 +0100

    Test alle ikoner

--- __tests__/Index/Icons.test.js
+++ __tests__/Index/Icons.test.js
@@ -11,6 +11,16 @@ describe("Icons ", () => {
     render(<Icons />);
   });
 
+  it("React laster inn og kan vises", () => {
+    const react = screen.getByLabelText("React ikon");
+    expect(react).toBeInTheDocument();
+  });
+
+  it("Vue laster inn og kan vises", () => {
+    const vue = screen.getByLabelText("Vue ikon");
+    expect(vue).toBeInTheDocument();
+  });
+
   it("Typescript laster inn og kan vises", () => {
     const typescript = screen.getByRole("img", { name: /typescript ikon/i });
     expect(typescript).toBeInTheDocument();
@@ -19,5 +29,10 @@ describe("Icons ", () => {
   it("Wordpress laster inn og kan vises", () => {
     const wordpress = screen.getByRole("img", { name: /wordpress ikon/i });
     expect(wordpress).toBeInTheDocument();
+  });
+
+  it("PHP laster inn og kan vises", () => {
+    const php = screen.getByLabelText("PHP  ikon");
+    expect(php).toBeInTheDocument();
   });
 });
