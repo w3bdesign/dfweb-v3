17d02486d50af78ebe8a9bd5bd833a73dd971ca0
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-27 20:05:29 +0100

    Prosjekterlistings tester

--- __tests__/Prosjekter/Prosjekterlistings.test.js
+++ __tests__/Prosjekter/Prosjekterlistings.test.js
@@ -4,15 +4,32 @@
 
 import { render, screen } from "@testing-library/react";
 
+import userEvent from "@testing-library/user-event";
+
 import ProsjekterListings from "../../src/components/Prosjekter/ProsjekterListings.component";
+
+import projects from "../../__mocks__/projects.json";
+import categories from "../../__mocks__/categories.json";
 
 describe("ProsjekterListings", () => {
   beforeEach(() => {
-    render(<ProsjekterListings />);
+    render(<ProsjekterListings projects={projects} categories={categories} />);
   });
 
   it("ProsjekterListings laster inn og kan vises", () => {
     const prosjekterlistings = screen.getByRole("main", { name: /innhold portefølje/i });
     expect(prosjekterlistings).toBeInTheDocument();
+  });
+
+  it("Vi laster inn 5 kategorier i kategorifilter", () => {
+    const kategorifilter = screen.getByRole("combobox", { name: /kategorifilter/i });
+    expect(kategorifilter.length).toBe(5);
+  });
+
+  it("Vi velger Typescript og tester at Typescript er valgt", () => {
+    const kategorifilter = screen.getByRole("combobox", { name: /kategorifilter/i });
+    const typescript = screen.getByRole("option", { name: "Typescript" });
+    userEvent.selectOptions(kategorifilter, typescript);
+    expect(typescript.selected).toBe(true);
   });
 });
