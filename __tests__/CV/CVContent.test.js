af2657179f435d23a3508bc5ec9fc5096f4036d7
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-27 03:25:56 +0100

    Mock react-pdf

--- __tests__/CV/CVContent.test.js
+++ __tests__/CV/CVContent.test.js
@@ -6,10 +6,27 @@ import { render, screen } from "@testing-library/react";
 
 import CVContent from "../../src/components/CV/CVContent.component";
 
+jest.mock("react-pdf", () => {
+  const Page = () => <div>cv</div>;
+
+  return {
+    pdfjs: {
+      GlobalWorkerOptions: {
+        workerSrc: "abc"
+      }
+    },
+    Outline: null,
+    Page,
+    Document: ({ onLoadSuccess = (pdf = { numPages: 2 }) => pdf.numPages }) => {
+      return <div>cv{onLoadSuccess({ numPages: 2 })}</div>;
+    }
+  };
+});
+
 describe("CVContent", () => {
-  it("CVContent laster inn og kan vises", () => {
+  it("CVContent laster inn og kan vises", async () => {
     render(<CVContent />);
-    const cvcontent = screen.getByText(/nøkkelkvalifikasjoner/i);
+    const cvcontent = await screen.findByText(/cv/i);
     expect(cvcontent).toBeInTheDocument();
   });
 });
