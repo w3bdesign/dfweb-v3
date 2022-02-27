793e049c7aeee7232335aed26056ceaf2887ab05
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-27 04:20:42 +0100

    Flere tester

--- __tests__/Animations/FadeDown.test.js
+++ __tests__/Animations/FadeDown.test.js
@@ -2,13 +2,14 @@
  * @jest-environment jsdom
  */
 
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import FadeDown from "../../src/components/Animations/FadeDown.component";
 
 describe("FadeDown", () => {
   it("FadeDown laster inn og kan vises", () => {
-    const { container } = render(<FadeDown delay={1}>FadeDown</FadeDown>);
-    expect(container).toMatchSnapshot();
+    render(<FadeDown delay={1}>FadeDown</FadeDown>);
+    const fadedown = screen.getByTestId("fadedown");
+    expect(fadedown).toBeInTheDocument();
   });
 });
