92da6cedc27ea89a042f10416f9cffb51319f33a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-27 03:51:08 +0100

    Update FadeUp.test.js

--- __tests__/Animations/FadeUp.test.js
+++ __tests__/Animations/FadeUp.test.js
@@ -8,7 +8,7 @@ import FadeUp from "../../src/components/Animations/FadeUp.component";
 
 describe("FadeUp", () => {
   it("FadeUp laster inn og kan vises", () => {
-    render(<FadeUp delay={1}>FadeDown</FadeUp>);
+    render(<FadeUp delay={1}>FadeUp</FadeUp>);
     const fadeup = screen.getByTestId("fadeup");
     expect(fadeup).toBeInTheDocument();
   });
