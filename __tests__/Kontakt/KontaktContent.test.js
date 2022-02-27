3c5ad7d4718332bdb3573ee3b87d396d1379d25a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-28 00:34:16 +0100

    Test kontaktskjema

---
+++ __tests__/Kontakt/KontaktContent.test.js
@@ -0,0 +1,40 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import KontaktContent from "../../src/components/Kontakt/KontaktContent.component";
+
+describe("KontaktContent", () => {
+  beforeEach(() => {
+    render(<KontaktContent />);
+  });
+
+  it("KontaktContent laster inn og kan vises", () => {
+    const kontaktcontent = screen.getByRole("heading", { name: /kontakt/i });
+    expect(kontaktcontent).toBeInTheDocument();
+  });
+
+  it("Test at vi kan skrive i navn tekstboks", () => {
+    const firstname = screen.getByRole("textbox", { name: /fullt navn/i });
+    userEvent.clear(firstname);
+    userEvent.type(firstname, "navn");
+    expect(firstname).toHaveValue("navn");
+  });
+
+  it("Test at vi kan skrive i telefon tekstboks", () => {
+    const telefon = screen.getByRole("textbox", { name: /telefonnummer \(i norskt format\)/i });
+    userEvent.clear(telefon);
+    userEvent.type(telefon, "telefon");
+    expect(telefon).toHaveValue("telefon");
+  });
+
+  it("Test at vi kan skrive i hva ønsker du å si tekstboks", () => {
+    const beskjed = screen.getByRole("textbox", { name: /hva ønsker du å si\?/i });
+    userEvent.clear(beskjed);
+    userEvent.type(beskjed, "beskjed");
+    expect(beskjed).toHaveValue("beskjed");
+  });
+});
--- __tests__/Prosjekter/Prosjekterlistings.test.js
+++ __tests__/Prosjekter/Prosjekterlistings.test.js
@@ -3,7 +3,6 @@
  */
 
 import { render, screen } from "@testing-library/react";
-
 import userEvent from "@testing-library/user-event";
 
 import ProsjekterListings from "../../src/components/Prosjekter/ProsjekterListings.component";
