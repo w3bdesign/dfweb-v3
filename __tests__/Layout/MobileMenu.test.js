70a43be998dc37fa885c57809f71a156dc083980
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-28 01:05:53 +0100

    Mobilemenu test

--- __tests__/Layout/MobileMenu.test.js
+++ __tests__/Layout/MobileMenu.test.js
@@ -7,9 +7,37 @@ import { render, screen } from "@testing-library/react";
 import MobileMenu from "../../src/components/Layout/MobileMenu.component";
 
 describe("MobileMenu", () => {
-  it("MobileMenu laster inn og kan vises", () => {
+  beforeEach(() => {
     render(<MobileMenu />);
+  });
+
+  it("MobileMenu laster inn og kan vises", () => {
     const mobilemenu = screen.getByTestId("mobile-menu");
     expect(mobilemenu).toBeInTheDocument();
+  });
+
+  it("Hjem linken eksisterer i menyen", () => {
+    const hjem = screen.getByText(/hjem/i);
+    expect(hjem).toBeInTheDocument();
+  });
+
+  it("Prosjekter linken eksisterer i menyen", () => {
+    const prosjekter = screen.getByText(/prosjekter/i);
+    expect(prosjekter).toBeInTheDocument();
+  });
+
+  it("CV linken eksisterer i menyen", () => {
+    const cv = screen.getByText(/cv/i);
+    expect(cv).toBeInTheDocument();
+  });
+
+  it("Github linken eksisterer i menyen", () => {
+    const github = screen.getByText(/github/i);
+    expect(github).toBeInTheDocument();
+  });
+
+  it("Kontakt linken eksisterer i menyen", () => {
+    const kontakt = screen.getByText(/kontakt/i);
+    expect(kontakt).toBeInTheDocument();
   });
 });
