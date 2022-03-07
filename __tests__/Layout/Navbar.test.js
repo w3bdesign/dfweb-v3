12572178d28f4d5b499a1003d0497b411f5008a9
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-07 14:49:57 +0100

    Mock useRouter

--- __tests__/Layout/Navbar.test.js
+++ __tests__/Layout/Navbar.test.js
@@ -2,15 +2,20 @@
  * @jest-environment jsdom
  */
 
- import { render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
- import Navbar from "../../src/components/Layout/Navbar.component";
- 
- describe("Navbar", () => {
-   it("Navbar laster inn og kan vises", () => {
-     render(<Navbar />);
-     const navbar = screen.getByRole('navigation')
-     expect(navbar).toBeInTheDocument();
-   });
- });
- 
+import Navbar from "../../src/components/Layout/Navbar.component";
+
+const useRouter = jest.spyOn(require("next/router"), "useRouter");
+
+describe("Navbar", () => {
+  it("Navbar laster inn og kan vises", () => {
+    useRouter.mockImplementationOnce(() => ({
+      query: { pathName: "/" }
+    }));
+
+    render(<Navbar />);
+    const navbar = screen.getByRole("navigation");
+    expect(navbar).toBeInTheDocument();
+  });
+});
