a0986a174e87e897b856c787dc521ba505165657
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2022-04-12 05:18:56 +0200

    Fiks skrivefeil

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from "next";
 //Components
 import Layout from "../components/Layout/Layout.component";
 
-const Custom404(): NextPage = () => (
+const Custom404: NextPage = () => (
   <Layout title="404">
     <h1>404 - side ikke funnet</h1>
   </Layout>
