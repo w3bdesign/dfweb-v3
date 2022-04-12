f4a918fe33c0b2a215df1b00e77775b31a61303f
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2022-04-12 05:09:27 +0200

    Standard 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,3 @@
-import React from 'react'
-import Error from 'next/error'
-
-const NotFoundPage = () => 
-
-   (
-      <Error
-        title={`"Error Page (404)" is not set in Sanity, or the page data is missing`}
-        statusCode="Data Error"
-      />
-    );
-
-
-export default NotFoundPage;
+export default function Custom404() {
+  return <h1>404 - Page Not Found</h1>
+}
