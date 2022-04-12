63d5853fc6c37605e19dd31f4a19f6c76aafb81c
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2022-04-12 05:17:18 +0200

    Implementer layout 404 side

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,13 @@
-export default function Custom404() {
-  return <h1>404 - side ikke funnet</h1>
-}
+// Types
+import type { NextPage } from "next";
+
+//Components
+import Layout from "../components/Layout/Layout.component";
+
+const Custom404(): NextPage = () => (
+  <Layout title="404">
+    <h1>404 - side ikke funnet</h1>
+  </Layout>
+);
+
+export default Custom404();
