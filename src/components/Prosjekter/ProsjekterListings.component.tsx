d27d757d9cbd5da85d09d2c17de37b391118eefa
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-04-16 02:54:35 +0200

    Opprydding og Sanity opprydding

--- src/components/Prosjekter/ProsjekterListings.component.tsx
+++ src/components/Prosjekter/ProsjekterListings.component.tsx
@@ -18,16 +18,25 @@ export interface IProject {
 
 export type TStringOrEmpty = string | undefined;
 
+export interface IProjectUrl {
+  _key: string;
+  _type: string;
+  external: boolean;
+  url: string;
+}
+
 export interface IProjectInterface {
   _id: string;
-  id: null | undefined | number;
+  id?: null | number;
   name: string;
   description: string;
   subdescription: string;
-  urlgithub: string;
-  urlwww: TStringOrEmpty;
+  urlgithub?: Array<IProjectUrl>;
+  urlwww?: Array<IProjectUrl>;
   category: string;
   projectimage: string;
+
+  projectCategory: any;
 }
 
 export interface IChangeEvent {
@@ -62,8 +71,7 @@ const ProsjekterListings = ({ projects, categories }: IProjectCategory): JSX.Ele
             <label
               className="p-2 mr-4 text-lg"
               aria-label="kategorifilter"
-              htmlFor="kategorifilter"
-            >
+              htmlFor="kategorifilter">
               Filtrer kategori:
             </label>
             <select
@@ -71,8 +79,7 @@ const ProsjekterListings = ({ projects, categories }: IProjectCategory): JSX.Ele
               name="kategorifilter"
               data-cy="kategorifilter"
               onChange={handleFilterChange}
-              className="w-40 p-2 bg-white text-black rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-            >
+              className="w-40 p-2 bg-white text-black rounded shadow appearance-none focus:outline-none focus:shadow-outline">
               <option label="" value="">
                 Ingen filtrering
               </option>
--- src/pages/prosjekter.tsx
+++ src/pages/prosjekter.tsx
@@ -16,18 +16,9 @@ const projectQuery = groq`*[_type == "project"]`;
 
 const categoryQuery = groq`*[_type == "category"]{ id, name } | order(id asc)`;
 
-const testQuery = groq`*[0]
-{
-  "prosjekt": *[_type == "project"]{name},
-  "kategori": *[
-    _type == "category"]{id, name} 
-  
-}`;
-
 const Prosjekter: NextPage = ({
   projects,
-  categories,
-  test
+  categories
 }: InferGetStaticPropsType<typeof getStaticProps>) => (
   <Layout title="Prosjekter">
     <ProsjekterListings projects={projects} categories={categories} />
@@ -38,15 +29,10 @@ export const getStaticProps: GetStaticProps = async () => {
   const projects = await getClient(false).fetch(projectQuery);
   const categories = await getClient(false).fetch(categoryQuery);
 
-  const test = await getClient(false).fetch(testQuery);
-
-  console.log("Test: ", test);
-
   return {
     props: {
       projects,
-      categories,
-      test
+      categories
     }
   };
 };
