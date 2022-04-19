967f6770fb5b0c5c11249c05062bfa87d4b93d0d
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-04-20 01:32:28 +0200

    Npm install

--- studio/sanity-codegen.config.ts
+++ studio/sanity-codegen.config.ts
@@ -1,8 +1,8 @@
-import { SanityCodegenConfig } from 'sanity-codegen';
+import { SanityCodegenConfig } from "sanity-codegen";
 
 const config: SanityCodegenConfig = {
-  schemaPath: './schemas/schema.js',
-  outputPath: './typeScriptschema.ts',
+  schemaPath: "./schemas/schema.js",
+  outputPath: "./typeScriptschema.ts"
 
   // NOTE: The CLI ships with a pre-configured babel config that shims out
   // the Sanity parts system. This babel config does not read from any
