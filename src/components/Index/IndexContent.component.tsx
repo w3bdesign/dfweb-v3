d0eee34e5dbb4f14df748a761ccc7cf1b6a0d638
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-04-19 22:17:15 +0200

    Formattering Prettier

--- src/components/Index/IndexContent.component.tsx
+++ src/components/Index/IndexContent.component.tsx
@@ -63,56 +63,47 @@ type TPageContent = { pagecontent: IPageContent[] };
  * @returns {JSX.Element} - Rendered component
  */
 
-const IndexContent = ({ pagecontent }: TPageContent): JSX.Element => {
-
-
-
-
-  return (
-    <main role="main" aria-label="Her kommer hovedinnholdet" id="maincontent">
-      <div className="mx-auto mt-16 rounded lg:mt-20 xl:mt-20 bg-graybg shadow-large md:mt-16 sm:mt-64 xs:mt-64">
-        {pagecontent && <Hero content={pagecontent[0].hero} />}
-        <div className="container grid gap-4 p-4 mx-auto mt-2 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 xs:grid-cols-1">
-          {pagecontent?.map(({ id, content }: IPageContent) => (
-            <Fragment key={id}>
-              {content?.map(({ _key, text, title }: IContent) => (
-                <section key={_key} aria-label={title} data-testid="sanity-section">
-                  <div className="mt-4 p-8 text-lg text-black bg-white rounded shadow min-h-full lg:h-128 xl:h-96">
-                    <BounceInScroll viewAmount={0}>
-                      <h2
-                        data-testid="sanity-title"
-                        data-cy={title}
-                        className="text-3xl text-center">
-                        {title}
-                      </h2>
-                      <br />
-                      <PortableText
-                        content={text}
-                        serializers={{
-                          code: ({ children }: ISerializerCode) => (
-                            <span className="mt-4 text-lg">
-                              {children}
-                              <br />
-                              &nbsp;
-                            </span>
-                          ),
-                          link: ({ children, href }: ISerializerLink) => (
-                            <Link href={href} passHref>
-                              <a className="underline">{children}</a>
-                            </Link>
-                          )
-                        }}
-                      />
-                    </BounceInScroll>
-                  </div>
-                </section>
-              ))}
-            </Fragment>
-          ))}
-        </div>
+const IndexContent = ({ pagecontent }: TPageContent): JSX.Element => (
+  <main role="main" aria-label="Her kommer hovedinnholdet" id="maincontent">
+    <div className="mx-auto mt-16 rounded lg:mt-20 xl:mt-20 bg-graybg shadow-large md:mt-16 sm:mt-64 xs:mt-64">
+      {pagecontent && <Hero content={pagecontent[0].hero} />}
+      <div className="container grid gap-4 p-4 mx-auto mt-2 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 xs:grid-cols-1">
+        {pagecontent?.map(({ id, content }: IPageContent) => (
+          <Fragment key={id}>
+            {content?.map(({ _key, text, title }: IContent) => (
+              <section key={_key} aria-label={title} data-testid="sanity-section">
+                <div className="mt-4 p-8 text-lg text-black bg-white rounded shadow min-h-full lg:h-128 xl:h-96">
+                  <BounceInScroll viewAmount={0}>
+                    <h2 data-testid="sanity-title" data-cy={title} className="text-3xl text-center">
+                      {title}
+                    </h2>
+                    <br />
+                    <PortableText
+                      content={text}
+                      serializers={{
+                        code: ({ children }: ISerializerCode) => (
+                          <span className="mt-4 text-lg">
+                            {children}
+                            <br />
+                            &nbsp;
+                          </span>
+                        ),
+                        link: ({ children, href }: ISerializerLink) => (
+                          <Link href={href} passHref>
+                            <a className="underline">{children}</a>
+                          </Link>
+                        )
+                      }}
+                    />
+                  </BounceInScroll>
+                </div>
+              </section>
+            ))}
+          </Fragment>
+        ))}
       </div>
-    </main>
-  );
-};
+    </div>
+  </main>
+);
 
 export default IndexContent;
