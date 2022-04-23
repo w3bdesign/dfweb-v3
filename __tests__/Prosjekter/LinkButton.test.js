b63d6a573321fea9cac7fca916e67465688e0a2d
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-04-24 00:23:10 +0200

    Fjern ubrukt kode

--- __tests__/Prosjekter/LinkButton.test.js
+++ __tests__/Prosjekter/LinkButton.test.js
@@ -3,7 +3,6 @@
  */
 
 import { render, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 
 import LinkButton from "../../src/components/Prosjekter/LinkButton.component";
 
