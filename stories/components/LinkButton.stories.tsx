c07e07f75ed83185b43194d83fb3c1aa245b37f0
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-04-27 22:47:19 +0200

    Fullfør arbeid med LinkButton

--- stories/components/LinkButton.stories.tsx
+++ stories/components/LinkButton.stories.tsx
@@ -6,16 +6,23 @@ import LinkButton from "../../src/components/Prosjekter/LinkButton.component";
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Components/Prosjekter/LinkButton",
-  component: LinkButton
+  component: LinkButton,
+  argTypes: {
+    url: { description: "URL address to link to" },
+    name: { description: "Aria label for the current element for accessibility reasons" },
+    text: { description: "Text to display in button" }
+  }
 } as ComponentMeta<typeof LinkButton>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof LinkButton> = () => (
-  <LinkButton
-    url="https://storybook.js.org/"
-    name="Github"
-    text="Github"
-  />
+const Template: ComponentStory<typeof LinkButton> = ({ url, name, text }) => (
+  <LinkButton url={url} name={name} text={text} />
 );
 
-export const Icon = Template;
+export const Button = Template;
+
+Button.args = {
+  url: "https://storybook.js.org",
+  name: "Github",
+  text: "Github"
+};
