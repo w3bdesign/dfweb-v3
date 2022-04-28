962612aa031571f364e2f4d5259e85aabed25f46
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-04-28 02:59:04 +0200

    Erstatt userEvent med user
    https://testing-library.com/docs/user-event/setup

--- __tests__/Kontakt/KontaktContent.test.js
+++ __tests__/Kontakt/KontaktContent.test.js
@@ -12,6 +12,8 @@ describe("KontaktContent", () => {
     render(<KontaktContent />);
   });
 
+  const user = userEvent.setup();
+
   it("KontaktContent laster inn", () => {
     const kontaktcontent = screen.getByRole("heading", { name: /kontakt/i });
     expect(kontaktcontent).toBeInTheDocument();
@@ -25,28 +27,29 @@ describe("KontaktContent", () => {
   it("Test at vi ikke kan sende et tomt skjema", async () => {
     const sendskjema = screen.getByRole("button", { name: /send skjema/i });
     const firstname = screen.getByRole("textbox", { name: /fullt navn/i });
-    await userEvent.click(sendskjema);
+
+    await user.click(sendskjema);
     expect(firstname).toBeVisible();
   });
 
   it("Test at vi kan skrive i navn tekstboks", async () => {
     const firstname = screen.getByRole("textbox", { name: /fullt navn/i });
-    await userEvent.clear(firstname);
-    await userEvent.type(firstname, "navn");
+    await user.clear(firstname);
+    await user.type(firstname, "navn");
     expect(firstname).toHaveValue("navn");
   });
 
   it("Test at vi kan skrive i telefon tekstboks", async () => {
     const telefon = screen.getByRole("textbox", { name: /telefonnummer \(i norskt format\)/i });
-    await userEvent.clear(telefon);
-    await userEvent.type(telefon, "telefon");
+    await user.clear(telefon);
+    await user.type(telefon, "telefon");
     expect(telefon).toHaveValue("telefon");
   });
 
   it("Test at vi kan skrive i hva ønsker du å si tekstboks", async () => {
     const beskjed = screen.getByRole("textbox", { name: /hva ønsker du å si\?/i });
-    await userEvent.clear(beskjed);
-    await userEvent.type(beskjed, "beskjed");
+    await user.clear(beskjed);
+    await user.type(beskjed, "beskjed");
     expect(beskjed).toHaveValue("beskjed");
   });
 });
