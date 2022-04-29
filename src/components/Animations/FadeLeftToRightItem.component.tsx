aa80762f2df066a091eae94274027490044d6ab2
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2022-04-30 01:58:54 +0200

    Endre X i animasjoner

--- src/components/Animations/FadeLeftToRightItem.component.tsx
+++ src/components/Animations/FadeLeftToRightItem.component.tsx
@@ -15,7 +15,7 @@ import { IAnimateProps } from "./types/Animations.types";
 const FadeLeftToRightItem = ({ children, cssClass }: IAnimateProps): JSX.Element => {
   const FadeLeftToRightItemVariants: Variants = {
     visible: { opacity: 1, x: 0 },
-    hidden: { opacity: 0, x: -50 }
+    hidden: { opacity: 0, x: -20 }
   };
   return (
     <motion.span
