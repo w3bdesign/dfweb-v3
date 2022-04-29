ff4889a7a996cd0b55cef12c50e5e68a28eeb0f5
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2022-04-30 01:57:37 +0200

    Endre Y i animasjoner

--- src/components/Animations/FadeDown.component.tsx
+++ src/components/Animations/FadeDown.component.tsx
@@ -15,7 +15,7 @@ import { IAnimateWithDelayProps } from "./types/Animations.types";
 
 const FadeDown = ({ children, cssClass, delay }: IAnimateWithDelayProps): JSX.Element => {
   const fadeDownVariants: Variants = {
-    initial: { opacity: 0, y: -50 },
+    initial: { opacity: 0, y: -20 },
     animate: {
       y: 0,
       opacity: 1,
