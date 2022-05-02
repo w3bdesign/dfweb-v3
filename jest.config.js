c9273f116f5a4e6eb524a2e8c8f6fd0ae6db4d3d
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-05-02 03:50:04 +0200

    Fjern testEnvironment

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testPathIgnorePatterns: ["<rootDir>/cypress/"],
-  testEnvironment: "jest-dom"
+  //testEnvironment: "jest-dom"
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
