168da9be981f9c85a76bee3cb3d410ecfcbbde45
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-05-03 19:22:01 +0200

    Update preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,14 @@
 import "../src/styles/globals.css";
 
+import * as NextImage from "next/image";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />
+});
+
 export const parameters = {
   layout: "centered",
   actions: { argTypesRegex: "^on[A-Z].*" },
