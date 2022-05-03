640f4a31680d34dc14e27f4e5cb8fca2b61c0856
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-05-03 19:28:18 +0200

    Fiks ErrorContent bilde Storybook

--- src/components/Error/ErrorContent.component.tsx
+++ src/components/Error/ErrorContent.component.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 import PageHeader from "../UI/PageHeader.component";
 
-import notfound from "../../../public/notfound.svg";
+
 
 interface IError {
   text: string;
@@ -27,7 +27,7 @@ const ErrorContent = ({ text }: IError): JSX.Element => (
             <div className="p-4 text-lg rounded">
               <div className="mt-0 sm:mt-2 xs:mt-2">
                 <div className="mt-64 sm:mt-12 w-72 mx-auto">
-                  <Image alt="Not found image" src={notfound} width="400" height="300" priority />
+                <Image alt="Not found image" src="/notfound.svg" width="400" height="300" priority />
                 </div>
                 <h2 className="mt-12 h-64 text-center sm:text-2xl text-lg">
                   Her var det noe rusk i maskineriet.
