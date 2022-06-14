33a4931ccdcb8ee79d51a25694967440261c9c26
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2022-06-14 05:42:11 +0200

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
-    // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require("./cypress/plugins/index.js")(on, config);
