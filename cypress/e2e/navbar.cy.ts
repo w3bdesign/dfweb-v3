391c694793c56afff4e8de54d64a790775eb670b
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-08-12 15:43:00 +0200

    Refaktorer Cypress test

--- cypress/e2e/navbar.cy.ts
+++ cypress/e2e/navbar.cy.ts
@@ -2,6 +2,7 @@
 
 describe("Test at navigasjon fungerer", () => {
   const navlink = ".navbar-link";
+  const mainUrl = "http://localhost:3000/";
 
   beforeEach(() => {
     cy.visit("/");
@@ -9,19 +10,19 @@ describe("Test at navigasjon fungerer", () => {
 
   it("Test at vi kan navigere til Hjem", () => {
     cy.get(navlink).eq(0).click();
-    cy.url().should("be.equal", "http://localhost:3000/");
+    cy.url().should("be.equal", `${mainUrl}`);
     cy.contains("Hei!");
   });
 
   it("Test at vi kan navigere til CV", () => {
     cy.get(navlink).eq(2).click();
-    cy.url().should("be.equal", "http://localhost:3000/cv");
+    cy.url().should("be.equal", `${mainUrl}/cv`);
     cy.contains("CV");
   });
 
   it("Test at vi kan navigere til Kontakt", () => {
     cy.get(navlink).eq(4).click();
-    cy.url().should("be.equal", "http://localhost:3000/kontakt");
+    cy.url().should("be.equal", `${mainUrl}/kontakt`);
     cy.contains("Kontakt");
   });
 });
