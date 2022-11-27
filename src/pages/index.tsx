93e28dcb3a227779d00a19869dff36c1fcbacd02
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-11-27 23:58:58 +0100

    Reverser fiks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 // Package imports
 import { groq } from "next-sanity";
 
-import { useState, useLayoutEffect } from "react";
-
 // Types
 import type { NextPage, GetStaticProps, InferGetStaticPropsType } from "next";
 
@@ -16,18 +14,11 @@ import { getClient } from "../lib/sanity.server";
 // Sanity GROQ queries
 const indexQuery = groq`*[_type == 'page' && title match 'Hjem']{"id": _id, title, hero, content}`;
 
-const Home: NextPage = ({ pagecontent }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const [mounted, setMounted] = useState(false);
-  useLayoutEffect(() => setMounted(true), []);
-
-  return (
-    <div className={`${!mounted ? "hidden" : ""}`}>
-      <Layout title="Forside">
-        <IndexContent pagecontent={pagecontent} />
-      </Layout>
-    </div>
-  );
-};
+const Home: NextPage = ({ pagecontent }: InferGetStaticPropsType<typeof getStaticProps>) => (
+  <Layout title="Forside">
+    <IndexContent pagecontent={pagecontent} />
+  </Layout>
+);
 
 export const getStaticProps: GetStaticProps = async () => {
   const pagecontent = await getClient(false).fetch(indexQuery);
