c26d9e0393ba5210b16e04b0bbe6c7ae6223ae9f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-11-27 01:44:38 +0100

    Update Hero.component.tsx

--- src/components/Index/Hero.component.tsx
+++ src/components/Index/Hero.component.tsx
@@ -22,17 +22,23 @@ const Hero = ({ content }: IContent): JSX.Element => (
     aria-label="Kontainer for animasjoner av introtekst"
     id="main-hero"
     data-testid="main-hero"
-    className="flex flex-col justify-center text-lg">
+    className="flex flex-col justify-center text-lg"
+  >
     <div className="mt-10 mb-4 bg-white p-2 opacity-75 md:mt-4 lg:mt-4 xl:mt-4">
       <div className="rounded text-black">
         <section>
           <FadeDown delay={0.5} cssClass="text-center">
             <span data-cy="hei" className="introtekst inline-block text-5xl">
+<<<<<<< HEAD
               {content.length ? content[0].text : null}
+=======
+              {content.length > 0 ? content[0].text : "Hei!"}
+>>>>>>> parent of 8a43ba967 (Opprydding Hero)
             </span>
           </FadeDown>
           <FadeUp
             delay={1.2}
+<<<<<<< HEAD
             cssClass="mt-4 px-6 text-lg md:mx-auto md:p-0 md:text-center md:text-xl lg:w-2/3 lg:p-0 lg:text-left lg:text-xl xl:p-0 xl:text-center xl:text-2xl">
             <h1>{content.length ? content[1].text : null}</h1>
           </FadeUp>
@@ -40,6 +46,17 @@ const Hero = ({ content }: IContent): JSX.Element => (
             delay={1.7}
             cssClass="mt-4 px-6 text-lg md:mx-auto md:p-0 md:text-center md:text-xl lg:w-2/3 lg:p-0 lg:text-left lg:text-xl xl:p-0 xl:text-center xl:text-2xl">
             <h2>{content.length ? content[2].text : null}</h2>
+=======
+            cssClass="mt-4 px-6 text-lg md:mx-auto md:p-0 md:text-center md:text-xl lg:w-2/3 lg:p-0 lg:text-left lg:text-xl xl:p-0 xl:text-center xl:text-2xl"
+          >
+            <h1>{content.length > 0 && content[1].text}</h1>
+          </FadeUp>
+          <FadeDown
+            delay={1.7}
+            cssClass="mt-4 px-6 text-lg md:mx-auto md:p-0 md:text-center md:text-xl lg:w-2/3 lg:p-0 lg:text-left lg:text-xl xl:p-0 xl:text-center xl:text-2xl"
+          >
+            <h2>{content.length > 0 && content[2].text}</h2>
+>>>>>>> parent of 8a43ba967 (Opprydding Hero)
           </FadeDown>
           <Icons />
         </section>
