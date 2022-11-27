b88123ba0fab323e13459703b6203ab5bcf04b1b
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-11-27 01:41:55 +0100

    Fiks bug

--- src/components/Index/Hero.component.tsx
+++ src/components/Index/Hero.component.tsx
@@ -28,18 +28,18 @@ const Hero = ({ content }: IContent): JSX.Element => (
         <section>
           <FadeDown delay={0.5} cssClass="text-center">
             <span data-cy="hei" className="introtekst inline-block text-5xl">
-              {content.length && content[0].text}
+              {content.length ? content[0].text : null}
             </span>
           </FadeDown>
           <FadeUp
             delay={1.2}
             cssClass="mt-4 px-6 text-lg md:mx-auto md:p-0 md:text-center md:text-xl lg:w-2/3 lg:p-0 lg:text-left lg:text-xl xl:p-0 xl:text-center xl:text-2xl">
-            <h1>{content.length && content[1].text}</h1>
+            <h1>{content.length ? content[1].text : null}</h1>
           </FadeUp>
           <FadeDown
             delay={1.7}
             cssClass="mt-4 px-6 text-lg md:mx-auto md:p-0 md:text-center md:text-xl lg:w-2/3 lg:p-0 lg:text-left lg:text-xl xl:p-0 xl:text-center xl:text-2xl">
-            <h2>{content.length && content[2].text}</h2>
+            <h2>{content.length ? content[2].text : null}</h2>
           </FadeDown>
           <Icons />
         </section>
