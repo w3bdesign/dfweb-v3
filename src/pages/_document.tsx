aa96ae468bd7de409de6bd75127c275f4c4f7c07
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-11-28 01:08:04 +0100

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,12 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
 
 class MyDocument extends Document {
- /* static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
-  }*/
+  }
 
- render() {
+  render(): JSX.Element {
     return (
       <Html lang="nb">
         <Head>
