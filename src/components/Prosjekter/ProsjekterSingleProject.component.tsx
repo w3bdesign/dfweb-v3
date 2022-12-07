db68e10f3806028ddd1a5f8381e2829ba233f303
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-12-07 05:28:17 +0100

    Line-clamp

--- src/components/Prosjekter/ProsjekterSingleProject.component.tsx
+++ src/components/Prosjekter/ProsjekterSingleProject.component.tsx
@@ -18,20 +18,18 @@ const ProsjekterSingleProject = ({ projects }: IProject): JSX.Element => (
   <div
     data-cy="prosjektgrid"
     data-testid="prosjektgrid"
-    className="grid gap-4 pt-4 pb-4 lg:px-0 xl:px-0 md:px-0 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 xs:grid-cols-1"
-  >
+    className="grid gap-4 pt-4 pb-4 lg:px-0 xl:px-0 md:px-0 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 xs:grid-cols-1">
     {projects.map(({ id, name, description, subdescription, urlwww, urlgithub, projectimage }) => (
       <div
         key={id}
         data-cy="projectdiv"
         data-testid="projectdiv"
-        className="p-6 text-lg text-black  bg-white rounded shadow"
-      >
+        className="p-6 text-lg text-black  bg-white rounded shadow">
         <BounceInScroll viewAmount={0.3}>
           <h2 className="text-xl font-black text-center">{name}</h2>
           <div className="mt-6 text-lg text-left lg:text-left md:text-left">
             <p className="min-h-[85px]">{description}</p>
-            <p className="mt-6 text-left md:text-left lg:text-left">
+            <p className="mt-6 text-left md:text-left lg:text-left line-clamp-1">
               Teknologier: {subdescription}
             </p>
             <div className="flex justify-center mt-6">
