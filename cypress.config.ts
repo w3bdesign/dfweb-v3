44d906ea63089402106ab32b514ece395241cc39
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-12-07 23:17:15 +0100

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,8 +7,6 @@ export default defineConfig({
   viewportHeight: 1080,
 
   e2e: {
-    // You may want to clean this up later by importing these.
-    setupNodeEvents() {},
     baseUrl: "http://localhost:3000"
   },
   component: {
