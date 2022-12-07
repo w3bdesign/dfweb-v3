060d650ac655faed6791c16982ed7d80c342b0b5
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-12-07 23:14:33 +0100

    Import Cypress plugins

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from "cypress";
 
+import "./cypress/plugins/index.js";
+
 export default defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
+
   e2e: {
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
-    },
+    setupNodeEvents() {},
     baseUrl: "http://localhost:3000"
+  },
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack"
+    }
   }
 });
