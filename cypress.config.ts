d3f37a99076052a9859eb79c89da04505ae33fd2
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-12-07 23:13:50 +0100

    Import Cypress plugins

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "cypress";
 
+import "./cypress/plugins/index.js";
+
 export default defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
 
   e2e: {
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
-    },
+    setupNodeEvents() {},
     baseUrl: "http://localhost:3000"
   },
   component: {
