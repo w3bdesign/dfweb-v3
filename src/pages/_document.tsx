49adce35ed65ccd973276c63f47bacf7640b7851
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-12-08 21:56:17 +0100

    Font display swap

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html lang="nb">
         <Head>
-         
+          <link rel="preconnect" href="https://fonts.googleapis.com?display=swap" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
