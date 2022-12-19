032dc294872499a448789c63ec105a60ab309054
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-12-19 06:26:51 +0100

    Prettier

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -25,15 +25,13 @@ import { mount } from "cypress/react18";
 // Alternatively, can be defined in cypress/support/component.d.ts
 // with a <reference path="./component" /> at the top of your spec.
 
-
-/*
 declare global {
   namespace Cypress {
     interface Chainable {
       mount: typeof mount;
     }
   }
-}*/
+}
 
 Cypress.Commands.add("mount", mount);
 
