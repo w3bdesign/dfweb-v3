77e7698227c291537eaf69cf4cb39677c3654c3b
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-12-19 06:28:25 +0100

    JS

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -25,6 +25,7 @@ import { mount } from "cypress/react18";
 // Alternatively, can be defined in cypress/support/component.d.ts
 // with a <reference path="./component" /> at the top of your spec.
 
+/*
 declare global {
   namespace Cypress {
     interface Chainable {
@@ -32,6 +33,7 @@ declare global {
     }
   }
 }
+*/
 
 Cypress.Commands.add("mount", mount);
 
