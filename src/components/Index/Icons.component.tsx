9e86ebcddfe49c97f9b6d0e3cc98d972a40b73a2
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-12-22 06:10:18 +0100

    Animasjonsfiks Hero

--- src/components/Index/Icons.component.tsx
+++ src/components/Index/Icons.component.tsx
@@ -12,24 +12,23 @@ import FadeLeftToRightItem from "../Animations/FadeLeftToRightItem.component";
  */
 
 const Icons = (): JSX.Element => (
-  <FadeLeftToRight delay={2.2} animateNotReverse cssClass="flex justify-center mt-4 p-2">
-    <div data-testid="icons" />
-    <FadeLeftToRightItem cssClass="p-2">
+  <div data-testid="icons" className="flex justify-center mt-4 p-2">
+    <span className="p-2">
       <FaReact title="React ikon" size="3em" aria-label="React ikon" />
-    </FadeLeftToRightItem>
-    <FadeLeftToRightItem cssClass="p-2">
+    </span>
+    <span className="p-2">
       <FaVuejs aria-label="Vue ikon" title="Vue ikon" size="3em" />
-    </FadeLeftToRightItem>
-    <FadeLeftToRightItem cssClass="p-2">
+    </span>
+    <span className="p-2">
       <SiTypescript title="Typescript ikon" size="3em" />
-    </FadeLeftToRightItem>
-    <FadeLeftToRightItem cssClass="p-2">
+    </span>
+    <span className="p-2">
       <SiWordpress title="Wordpress ikon" size="3em" />
-    </FadeLeftToRightItem>
-    <FadeLeftToRightItem cssClass="p-2">
+    </span>
+    <span className="p-2">
       <FaPhp aria-label="PHP ikon" title="PHP ikon" size="3em" />
-    </FadeLeftToRightItem>
-  </FadeLeftToRight>
+    </span>
+  </div>
 );
 
 export default Icons;
