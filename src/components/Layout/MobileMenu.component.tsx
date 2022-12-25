da2029161ee2c8cfd3f89f5a0f5174d1353ccdbe
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-12-25 03:42:38 +0100

    Npm refresh

--- src/components/Layout/MobileMenu.component.tsx
+++ src/components/Layout/MobileMenu.component.tsx
@@ -55,7 +55,8 @@ const MobileMenu = (): JSX.Element => {
         data-testid="mobile-menu"
         data-cy="mobile-menu"
         aria-hidden={!isExpanded}
-        className="absolute right-0 w-full text-center bg-gray-800 mt-4 w-30">
+        className="absolute right-0 w-full text-center bg-gray-800 mt-4 w-30"
+      >
         <AnimatePresence>
           {isExpanded && (
             <motion.aside
@@ -68,7 +69,8 @@ const MobileMenu = (): JSX.Element => {
               exit={{
                 height: 0,
                 transition: { delay: 0.15, duration: 1.6, ease: "easeInOut" }
-              }}>
+              }}
+            >
               <nav aria-label="Navigasjon">
                 <motion.div initial="closed" animate="open" exit="closed" variants={sideVariants}>
                   <ul>
@@ -77,14 +79,16 @@ const MobileMenu = (): JSX.Element => {
                         key={id}
                         className="block p-4 text-xl text-white hover:underline mx-auto text-center border-t border-b border-gray-600 border-solid shadow"
                         data-cy="mobile-menu-item"
-                        variants={itemVariants}>
+                        variants={itemVariants}
+                      >
                         {external ? (
                           <a
                             aria-label={text}
                             href={url}
                             target="_blank"
                             rel="noreferrer"
-                            data-testid={`mobil-${text}`}>
+                            data-testid={`mobil-${text}`}
+                          >
                             {text}
                           </a>
                         ) : (
