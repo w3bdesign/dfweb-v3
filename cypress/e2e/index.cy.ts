41d2dcab0d2c44ed9cd13b7e14c2cd63e62adbbc
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-10 00:57:39 +0100

    Test Applitools

--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -4,10 +4,24 @@
 describe("Forside", () => {
   beforeEach(() => {
     cy.visit("/");
+
+    // Open Eyes to start visual testing.
+    // Each test should open its own Eyes for its own snapshots.
+    cy.eyesOpen({
+      appName: "ACME Bank", // The name of the app under test
+      testName: Cypress.currentTest.title // The name of the test case
+    });
   });
 
   it(`Hei vises på forsiden`, () => {
     cy.get('[data-cy="hei"]').contains("Hei!");
+
+    // Verify the full login page loaded correctly.
+    cy.eyesCheckWindow({
+      tag: "Index page",
+      target: "window",
+      fully: true
+    });
   });
 
   it(`Om meg vises på forsiden`, () => {
@@ -16,6 +30,12 @@ describe("Forside", () => {
 
   it(`Prosjekter vises på forsiden`, () => {
     cy.get('[data-cy="Prosjekter"]').contains("Prosjekter");
+  });
+
+  // This method performs cleanup after each test.
+  afterEach(() => {
+    // Close Eyes to tell the server it should display the results.
+    cy.eyesClose();
   });
 });
 
