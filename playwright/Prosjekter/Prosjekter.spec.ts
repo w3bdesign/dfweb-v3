3aa3cd7034d8a1f863d15a892a03c2b7bc154040
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-17 20:12:32 +0100

    Test accessibility

--- playwright/Prosjekter/Prosjekter.spec.ts
+++ playwright/Prosjekter/Prosjekter.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test.describe("Prosjekter", () => {
   test.beforeEach(async ({ page }) => {
@@ -15,5 +16,14 @@ test.describe("Prosjekter", () => {
     const project = page.getByTestId("projectdiv");
     const count = await project.count();    
     await expect(count).toBeGreaterThan(0);
+  });
+
+  test("Har ingen accessibility issues", async ({ page }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    const feil = accessibilityScanResults.violations;
+    if (feil.length > 0) {
+      await page.screenshot({ path: "screenshot.png" });
+    }
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
