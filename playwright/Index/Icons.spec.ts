ac81ad1aaec102684299aefede9ba7722ef6c662
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-17 03:17:33 +0100

    Test ikoner

--- playwright/Index/Icons.spec.ts
+++ playwright/Index/Icons.spec.ts
@@ -1,15 +1,29 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Icons", () => {
+  const url = "http://localhost:3000/";
+
   test("It should render icons", async ({ page }) => {
-    await page.goto("http://localhost:3000/");
+    await page.goto(url);
     const icons = page.getByTestId("icons");
     await expect(icons).toBeVisible();
   });
 
   test("It should render React icon", async ({ page }) => {
-    await page.goto("http://localhost:3000/");
-    const react = page.getByTestId("Ikke react");
+    await page.goto(url);
+    const react = page.getByTestId("React");
     await expect(react).toBeVisible();
+  });
+
+  test("It should render Vue icon", async ({ page }) => {
+    await page.goto(url);
+    const vue = page.getByTestId("Vue");
+    await expect(vue).toBeVisible();
+  });
+
+  test("It should render Typescript icon", async ({ page }) => {
+    await page.goto(url);
+    const typescript = page.getByTestId("Typescript");
+    await expect(typescript).toBeVisible();
   });
 });
