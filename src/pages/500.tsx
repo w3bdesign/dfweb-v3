2478b17abdb9cb12f18d138c3a996c7a1cf3f07f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-05 05:17:35 +0100

    Fiks error sidene

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,14 +1,33 @@
+// Package imports
+import { groq } from "next-sanity";
+
 // Types
-import type { NextPage } from "next";
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
+
+// Utilities
+import { getClient } from "../lib/sanity.server";
 
 //Components
 import Layout from "../components/Layout/Layout.component";
 import ErrorContent from "../components/Error/ErrorContent.component";
 
-const Custom500: NextPage = () => (
-  <Layout title="Feil - 500">
+// Sanity GROQ queries
+const navigationQuery = groq`*[_type == 'Links']{id, Text, Url} | order(id asc)`;
+
+const Custom500: NextPage = ({ navigation }: InferGetStaticPropsType<typeof getStaticProps>) => (
+  <Layout title="Feil - 500" links={navigation}>
     <ErrorContent text="Feil - 500" />
   </Layout>
 );
+
+export const getStaticProps: GetStaticProps = async () => {
+  const navigation = await getClient(false).fetch(navigationQuery);
+
+  return {
+    props: {
+      navigation
+    }
+  };
+};
 
 export default Custom500;
