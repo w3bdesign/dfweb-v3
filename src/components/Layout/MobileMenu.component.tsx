384a9bb6358b8d8edca7ccc729111a92d3b3eb46
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-05 05:47:44 +0100

    Fiks mobilmeny

--- src/components/Layout/MobileMenu.component.tsx
+++ src/components/Layout/MobileMenu.component.tsx
@@ -4,9 +4,18 @@ import Link from "next/link";
 
 import { AnimatePresence, useCycle, motion } from "framer-motion";
 
-import LINKS from "../../utils/constants/LINKS";
-
 import Hamburger from "./Hamburger.component";
+
+interface ILinks {
+  Text: string;
+  Url: string;
+  id: number;
+  External: boolean;
+}
+
+interface IMobileMenuProps {
+  links: ILinks[];
+}
 
 /**
  * Renders the mobile menu.
@@ -15,7 +24,7 @@ import Hamburger from "./Hamburger.component";
  * @returns {JSX.Element} - Rendered component
  */
 
-const MobileMenu = (): JSX.Element => {
+const MobileMenu = ({ links }: IMobileMenuProps) => {
   const [isExpanded, setisExpanded] = useCycle<boolean>(false, true);
   const ref = useRef(null);
 
@@ -55,8 +64,7 @@ const MobileMenu = (): JSX.Element => {
         data-testid="mobile-menu"
         data-cy="mobile-menu"
         aria-hidden={!isExpanded}
-        className="absolute right-0 w-full text-center bg-gray-800 mt-4 w-30"
-      >
+        className="absolute right-0 w-full text-center bg-gray-800 mt-4 w-30">
         <AnimatePresence>
           {isExpanded && (
             <motion.aside
@@ -69,31 +77,28 @@ const MobileMenu = (): JSX.Element => {
               exit={{
                 height: 0,
                 transition: { delay: 0.15, duration: 1.6, ease: "easeInOut" }
-              }}
-            >
+              }}>
               <nav aria-label="Navigasjon">
                 <motion.div initial="closed" animate="open" exit="closed" variants={sideVariants}>
                   <ul>
-                    {LINKS.map(({ id, url, text, external }) => (
+                    {links.map(({ id, Text, Url, External }) => (
                       <motion.li
                         key={id}
                         className="block p-4 text-xl text-white hover:underline mx-auto text-center border-t border-b border-gray-600 border-solid shadow"
                         data-cy="mobile-menu-item"
-                        variants={itemVariants}
-                      >
+                        variants={itemVariants}>
                         {external ? (
                           <a
-                            aria-label={text}
-                            href={url}
+                            aria-label={Text}
+                            href={Url}
                             target="_blank"
                             rel="noreferrer"
-                            data-testid={`mobil-${text}`}
-                          >
-                            {text}
+                            data-testid={`mobil-${Text}`}>
+                            {Text}
                           </a>
                         ) : (
-                          <Link data-testid={`mobil-${text}`} href={url}>
-                            {text}
+                          <Link data-testid={`mobil-${Text}`} href={Url}>
+                            {Text}
                           </Link>
                         )}
                       </motion.li>
