bafad653e24f1e53da0a4091b97674e8eb907b56
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-06 03:51:59 +0100

    DRY i test

--- __tests__/Layout/Navbar.test.tsx
+++ __tests__/Layout/Navbar.test.tsx
@@ -11,24 +11,20 @@ const useRouter = jest.spyOn(require("next/router"), "useRouter");
 import linksmock from "../../__mocks__/links.json";
 
 describe("Navbar", () => {
-  it("Navbar laster inn og kan vises", () => {
+  beforeEach(() => {
     useRouter.mockImplementationOnce(() => ({
       query: { pathName: "/" }
     }));
 
     render(<Navbar links={linksmock} />);
+  });
 
+  it("Navbar laster inn og kan vises", () => {
     const navbar = screen.getByRole("navigation");
     expect(navbar).toBeInTheDocument();
   });
 
   it("Link Hjem har navbar-link class", () => {
-    useRouter.mockImplementationOnce(() => ({
-      query: { pathName: "/" }
-    }));
-
-    render(<Navbar links={linksmock} />);
-
     const link = screen.getByRole("link", { name: /hjem/i });
 
     expect(link.className).toBe(
@@ -37,31 +33,18 @@ describe("Navbar", () => {
   });
 
   it("Link Github har riktig klasse", () => {
-    useRouter.mockImplementationOnce(() => ({
-      query: { pathName: "/" }
-    }));
-
-    render(<Navbar links={linksmock} />);
-
     const github = screen.getByRole("link", { name: /github/i });
 
     expect(github.className).toBe("navbar-link inline-block text-xl text-white");
   });
 
   it("Test activeLink", () => {
-
-    useRouter.mockImplementationOnce(() => ({
-      query: { pathName: "/" }
-    }));
-
     const activeLink = jest.fn((url: string, pathname: string) => {
       if (pathname === url) {
         return "navbar-link-active";
       }
       return "";
     });
-
-    render(<Navbar links={linksmock} />);
 
     const url = "/home";
     const pathname = "/home";
