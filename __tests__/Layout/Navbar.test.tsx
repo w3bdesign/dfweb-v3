2073ddfc680151ab7b13d2e911191085072d9cf3
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-06 03:13:52 +0100

    Test

--- __tests__/Layout/Navbar.test.tsx
+++ __tests__/Layout/Navbar.test.tsx
@@ -8,6 +8,10 @@ import Navbar from "../../src/components/Layout/Navbar.component";
 
 const useRouter = jest.spyOn(require("next/router"), "useRouter");
 
+//import { useRouter } from "next/router";
+
+//jest.mock("next/router");
+
 import linksmock from "../../__mocks__/links.json";
 
 describe("Navbar", () => {
@@ -31,5 +35,12 @@ describe("Navbar", () => {
     expect(navbar).toBeInTheDocument();
 
     expect(activeLink.mock.calls.length).toBe(1);
+
+    const activeClass = Navbar({ links: [{ Text: "Home", Url: "/", id: 1, External: false }] })
+      .props.children[1].props.children[1].props.children[1].props.className;
+
+    expect(activeClass).toEqual(
+      "navbar-link eds-top-navigation-item inline-block text-xl text-white navbar-link-active"
+    );
   });
 });
