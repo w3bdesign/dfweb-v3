315eb1bdfe4a555ff2f7261a5acaf81640eed81a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-06 02:26:35 +0100

    Update CV.spec.ts

--- playwright/CV/CV.spec.ts
+++ playwright/CV/CV.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
-import AxeBuilder from "@axe-core/playwright";
+
+import { accessibilityFixture } from "../helpers/helper";
 
 test.describe("CV", () => {
   test.beforeEach(async ({ page }) => {
@@ -27,12 +28,5 @@ test.describe("CV", () => {
     await expect(download).toBeVisible();
   });
 
-  test("Har ingen accessibility issues", async ({ page }) => {
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-    const feil = accessibilityScanResults.violations;
-    if (feil.length > 0) {
-      await page.screenshot({ path: "screenshot.png" });
-    }
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
+  accessibilityFixture();
 });
