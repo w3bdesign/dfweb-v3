bb9b31a2ed7a5bf3266585f84e7461bed5a8f8ee
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-14 18:30:52 +0100

    Forbedre Button komponent

--- src/components/UI/Button.component.tsx
+++ src/components/UI/Button.component.tsx
@@ -1,11 +1,10 @@
-import type { ReactNode } from "react";
-
-type TRenderAs = "button" | "a";
+import type { ElementType, ReactNode } from "react";
 
 export interface IButtonProps {
-  children?: ReactNode | JSX.Element;
+  children: ReactNode;
   href?: string;
-  renderAs?: TRenderAs;
+  renderAs?: ElementType;
+  type?: "button" | "submit" | "reset" | undefined;
 }
 
 /**
@@ -13,17 +12,18 @@ export interface IButtonProps {
  *
  * @param {ReactNode | JSX.Element} children - The content of the button.
  * @param {string} href - The link to navigate to when the button is clicked.
- * @param {TRenderAs} renderAs - The HTML tag to render the button as.
+ * @param {ElementType} renderAs - The HTML tag to render the button as.
+ * @param {string} type - The type of button (default is "submit")
  * @returns {JSX.Element} - A button component.
  */
 
-const Button = ({ children, href, renderAs, ...props }: IButtonProps) => {
+const Button = ({ children, href, renderAs, type = "submit", ...props }: IButtonProps) => {
   const Component = renderAs || "button";
   const targetLink = renderAs ? "_blank" : undefined;
 
   return (
     <Component
-      type="submit"
+      type={type}
       data-cy="submit"
       className="p-4 m-4 text-white transition duration-500 ease-in-out bg-gray-800 rounded hover:shadow-outline hover:bg-gray-600"
       href={href}
