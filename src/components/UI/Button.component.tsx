24f4fff82bd729c4ca5e8b2f93807f07a5c55374
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-14 05:49:48 +0100

    Polymorphic <Button>

--- src/components/UI/Button.component.tsx
+++ src/components/UI/Button.component.tsx
@@ -1,30 +1,33 @@
-import type { ReactNode, MouseEventHandler } from "react";
+import type { ReactNode } from "react";
 
 export interface IButtonProps {
-  text?: string;
   children?: ReactNode | JSX.Element;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
+  href?: string;
+  renderAs?: "button" | "a";
 }
 
 /**
  * Button component usually used for links
  * @function Button
- * @param {string?} text - Text to show in the button
+ *
  * @param {children?} children - HTML children to render in the button
  * @param {MouseEventHandler<HTMLButtonElement>?} onClick - onClick handler for button
  * @returns {JSX.Element} - Rendered component
  */
 
-const Button = ({ text, children, onClick }: IButtonProps)  => (
-  <button
-    type="submit"
-    onClick={onClick}
-    data-cy="submit"
-    className="p-4 m-4 text-white transition duration-500 ease-in-out bg-gray-800 rounded hover:shadow-outline hover:bg-gray-600"
-  >
-    {text}
-    {children}
-  </button>
-);
+const Button = ({ children, href, renderAs, ...props }: IButtonProps) => {
+  const Component = renderAs || "button";
+
+  return (
+    <Component
+      type="submit"
+      data-cy="submit"
+      className="p-4 m-4 text-white transition duration-500 ease-in-out bg-gray-800 rounded hover:shadow-outline hover:bg-gray-600"
+      href={href}
+      {...props}>
+      {children}
+    </Component>
+  );
+};
 
 export default Button;
