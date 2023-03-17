34f8d8484cecde289146578c206b9c84d917c80e
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-17 05:57:51 +0100

    Update KontaktContent.test.tsx

--- __tests__/Kontakt/KontaktContent.test.tsx
+++ __tests__/Kontakt/KontaktContent.test.tsx
@@ -2,73 +2,6 @@
  * @jest-environment jsdom
  */
 
-/*
-
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-
-import KontaktContent from "../../src/components/Kontakt/KontaktContent.component";
-
-describe("KontaktContent", () => {
-  const user = userEvent.setup();
-
-  beforeEach(() => {
-    render(<KontaktContent />);
-  });
-
-  it("KontaktContent laster inn", () => {
-    const kontaktcontent = screen.getByRole("heading", { name: /kontakt/i });
-    expect(kontaktcontent).toBeInTheDocument();
-  });
-
-  it("KontaktContent laster inn og kan vises", () => {
-    const kontaktcontent = screen.getByRole("heading", { name: /kontakt/i });
-    expect(kontaktcontent).toBeVisible();
-  });
-
-  it("KontaktContent hovedinnhold laster inn", () => {
-    const kontaktmaincontent = screen.getByTestId("kontaktcontent");
-    expect(kontaktmaincontent).toBeInTheDocument();
-  });
-
-  it("KontaktContent hovedinnhold laster inn og kan vises", () => {
-    const kontaktmaincontent = screen.getByTestId("kontaktcontent");
-    expect(kontaktmaincontent).toBeVisible();
-  });
-
-  it("Test at vi ikke kan sende et tomt skjema", async () => {
-    const sendskjema = screen.getByRole("button", { name: /send skjema/i });
-    const firstname = screen.getByRole("textbox", { name: /fullt navn/i });
-
-    await user.click(sendskjema);
-    expect(firstname).toBeVisible();
-  });
-
-  it("Test at vi kan skrive i navn tekstboks", async () => {
-    const firstname = screen.getByRole("textbox", { name: /fullt navn/i });
-    await user.clear(firstname);
-    await user.type(firstname, "navn");
-    expect(firstname).toHaveValue("navn");
-  });
-
-  it("Test at vi kan skrive i telefon tekstboks", async () => {
-    const telefon = screen.getByRole("textbox", { name: /telefonnummer \(i norskt format\)/i });
-    await user.clear(telefon);
-    await user.type(telefon, "telefon");
-    expect(telefon).toHaveValue("telefon");
-  });
-
-  it("Test at vi kan skrive i hva ønsker du å si tekstboks", async () => {
-    const beskjed = screen.getByRole("textbox", { name: /hva ønsker du å si\?/i });
-    await user.clear(beskjed);
-    await user.type(beskjed, "beskjed");
-    expect(beskjed).toHaveValue("beskjed");
-  });
-});
-*/
-
-////////////////////
-
 import { fireEvent, render, screen } from "@testing-library/react";
 import KontaktContent from "../../src/components/Kontakt/KontaktContent.component";
 
@@ -79,30 +12,23 @@ describe("KontaktContent", () => {
   });
 
   test("submits the form and displays success message", async () => {
-
-
     render(<KontaktContent />);
 
     // fill out form fields
     fireEvent.change(screen.getByLabelText("Fullt navn"), {
-      target: { value: "Test User" },
+      target: { value: "Test User" }
     });
     fireEvent.change(screen.getByLabelText("Telefonnummer (i norskt format)"), {
-      target: { value: "12345678" },
+      target: { value: "12345678" }
     });
     fireEvent.change(screen.getByLabelText("Hva ønsker du å si?"), {
-      target: { value: "Test message" },
+      target: { value: "Test message" }
     });
 
     // submit form
     fireEvent.click(screen.getByText("Send skjema"));
 
-    // wait for server response
-    //await screen.findByText("Takk for din beskjed");
-
     // assert success message is displayed
     expect(screen.getByText("Fullt navn")).toBeInTheDocument();
   });
-
-  
 });
