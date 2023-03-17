3ed06d54af2c121df4b7a7efd6c31406a91e836e
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-17 06:36:24 +0100

    Update Grow.test.tsx

--- __tests__/Animations/Grow.test.tsx
+++ __tests__/Animations/Grow.test.tsx
@@ -1,29 +1,29 @@
 /**
  * @jest-environment jsdom
  */
-////////////////
 
 import { render, screen } from "@testing-library/react";
 import Grow from "../../src/components/Animations/Grow.component";
 
 describe("Grow", () => {
-  test("renders the component", () => {
-    render(<Grow>Hello World</Grow>);
-    expect(screen.getByTestId("grow")).toBeInTheDocument();
-  });
-
-  test("applies the correct animation properties", () => {
+  beforeEach(() => {
     render(
       <Grow duration={0.3} delay={0.2} easing={[0.42, 0, 0.58, 1]}>
         Hello World
       </Grow>
     );
+  });
+
+  test("renders the component", () => {
+    expect(screen.getByTestId("grow")).toBeInTheDocument();
+  });
+
+  test("applies the correct animation properties", () => {
     const element = screen.getByTestId("grow");
     expect(element).toHaveStyle("transform: scale(0) translateZ(0);");
   });
 
   test("renders its children", () => {
-    render(<Grow>Hello World</Grow>);
     expect(screen.getByText("Hello World")).toBeInTheDocument();
   });
 });
