cac921a551064624224313d83ffe963498a711cc
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-17 06:34:02 +0100

    Update Grow.test.tsx

--- __tests__/Animations/Grow.test.tsx
+++ __tests__/Animations/Grow.test.tsx
@@ -1,23 +1,29 @@
 /**
  * @jest-environment jsdom
  */
+////////////////
 
 import { render, screen } from "@testing-library/react";
-
 import Grow from "../../src/components/Animations/Grow.component";
 
 describe("Grow", () => {
-  beforeEach(() => {
-    render(<Grow delay={1}>Grow</Grow>);
+  test("renders the component", () => {
+    render(<Grow>Hello World</Grow>);
+    expect(screen.getByTestId("grow")).toBeInTheDocument();
   });
 
-  it("FadeUp laster inn og kan vises", () => {
-    const grow = screen.getByTestId("grow");
-    expect(grow).toBeInTheDocument();
+  test("applies the correct animation properties", () => {
+    render(
+      <Grow duration={0.3} delay={0.2} easing={[0.42, 0, 0.58, 1]}>
+        Hello World
+      </Grow>
+    );
+    const element = screen.getByTestId("grow");
+    expect(element).toHaveStyle("transform: scale(0) translateZ(0);");
   });
 
-  it("Framer motion setter translateY", () => {
-    const grow = screen.getByTestId("grow");
-    expect(grow).toContainHTML("translateZ");
+  test("renders its children", () => {
+    render(<Grow>Hello World</Grow>);
+    expect(screen.getByText("Hello World")).toBeInTheDocument();
   });
 });
