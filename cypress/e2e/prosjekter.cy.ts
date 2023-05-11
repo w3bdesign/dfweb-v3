f939d82e5be715e273fcc9588082957baec8a16c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-11 21:20:45 +0200

    Oppdater PHP test

--- cypress/e2e/prosjekter.cy.ts
+++ cypress/e2e/prosjekter.cy.ts
@@ -9,9 +9,9 @@ describe("Test at prosjekter vises og at filter fungerer", () => {
     cy.visit("/prosjekter");
   });
 
-  it("Velg PHP og se at vi bare får ett resultat", () => {
+  it("Velg PHP og se at vi bare får to resultater", () => {
     cy.get('[data-cy="kategorifilter"]').select("PHP");
-    cy.get('[data-cy="prosjektgrid"]').find('[data-cy="projectdiv"]').should("have.length", 1);
+    cy.get('[data-cy="prosjektgrid"]').find('[data-cy="projectdiv"]').should("have.length", 2);
   });
 
   it("Prosjekter skal ikke ha noen a11y feilmeldinger", () => {
