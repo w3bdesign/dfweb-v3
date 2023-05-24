91d67334f643fb82216f1c21326c6241f90b6e87
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-25 01:36:09 +0200

    Update CVContent.component.tsx

--- src/components/CV/CVContent.component.tsx
+++ src/components/CV/CVContent.component.tsx
@@ -7,12 +7,7 @@ import PageHeader from "../UI/PageHeader.component";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
-//pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.js",
-  import.meta.url
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 /**
  * Renders CV content
