4341fd587b162107095f3b8a4c272af99fe32633
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-28 06:23:03 +0200

    Revert "Update PDFViewer.component.tsx"
    This reverts commit fe06e834eae1de7b646952211dd1ae43436932c6.

--- src/components/CV/PDFViewer.component.tsx
+++ src/components/CV/PDFViewer.component.tsx
@@ -1,5 +1,7 @@
 import { type FC } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
