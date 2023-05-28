fe06e834eae1de7b646952211dd1ae43436932c6
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-28 06:11:34 +0200

    Update PDFViewer.component.tsx

--- src/components/CV/PDFViewer.component.tsx
+++ src/components/CV/PDFViewer.component.tsx
@@ -1,7 +1,5 @@
 import { type FC } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import "react-pdf/dist/esm/Page/TextLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
