f888aca167745e3d8f3411f05a6c5fab424a5988
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-07-21 15:55:53 +0200

    Kommenter mock funksjoner

--- __mocks__/react-dom.ts
+++ __mocks__/react-dom.ts
@@ -2,7 +2,7 @@ const reactDom = jest.requireActual("react-dom");
 
 module.exports = {
   ...reactDom, // use actual for all non-hook parts
-  preload: () => {},
-  preconnect: () => {},
-  prefetchDNS: () => {}
+  preload: () => {}, // This is a mock function used in testing, so it's intentionally empty.
+  preconnect: () => {}, // This is a mock function used in testing, so it's intentionally empty.
+  prefetchDNS: () => {} // This is a mock function used in testing, so it's intentionally empty.
 };
