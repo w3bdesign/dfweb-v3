07afc5bd249c5b1759aa63e5175e60d579bd266c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-10-04 02:32:23 +0200

    Fiks footer

--- src/components/Layout/Footer.component.tsx
+++ src/components/Layout/Footer.component.tsx
@@ -10,12 +10,14 @@ import { BiCopyright } from "react-icons/bi";
 const Footer = () => (
   <footer aria-label="Innholdet for bunnteksten med copyright" data-testid="footer">
     <div className="mt-10 bg-gray-800 shadow">
-      <div className="w-full mx-auto p-6 text-center text-white font-semibold inline-block">
-        Copyright Daniel Fjeldstad
-        <IconContext.Provider value={{ className: "inline-block m-2" }}>
-          <BiCopyright size="1.2em" />
-        </IconContext.Provider>
-        {new Date().getFullYear()}
+      <div className="w-full mx-auto p-6 text-center text-white font-semibold">
+        <div className="flex justify-center items-center space-x-2">
+          <span>Copyright Daniel Fjeldstad</span>
+          <IconContext.Provider value={{ className: "inline-block" }}>
+            <BiCopyright size="1.2em" />
+          </IconContext.Provider>
+          <span>{new Date().getFullYear()}</span>
+        </div>
       </div>
     </div>
   </footer>
