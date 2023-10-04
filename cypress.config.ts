2d50704ce4be7915a74b1c14a065776eba17cb37
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-10-04 05:50:50 +0200

    Fiks test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from "cypress";
 
-import "./cypress/plugins/index.js";
+//import "./cypress/plugins/index.js";
+
+import { setupNodeEvents as setupPlugins } from "./cypress/plugins/index.js";
 
 export default defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
-    setupNodeEvents(_on, config) {
+    setupNodeEvents(on, config) {
       // e2e testing node events setup code
+      setupPlugins(on, config); // Register the 'task' event from your plugins file
       return config;
     },
     baseUrl: "http://localhost:3000"
