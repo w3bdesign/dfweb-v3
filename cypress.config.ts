f4391585614b11dc933d5c6846778e918585143f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-10-04 05:53:05 +0200

    Revert "Fiks test"
    This reverts commit 2d50704ce4be7915a74b1c14a065776eba17cb37.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from "cypress";
 
-//import "./cypress/plugins/index.js";
-
-import { setupNodeEvents as setupPlugins } from "./cypress/plugins/index.js";
+import "./cypress/plugins/index.js";
 
 export default defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(_on, config) {
       // e2e testing node events setup code
-      setupPlugins(on, config); // Register the 'task' event from your plugins file
       return config;
     },
     baseUrl: "http://localhost:3000"
