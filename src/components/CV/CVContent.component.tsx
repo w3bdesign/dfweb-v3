21c70df11bd91c9645f5d49c76247aebcd8dff18
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-06-09 21:09:28 +0200

    Update CVContent.component.tsx

--- src/components/CV/CVContent.component.tsx
+++ src/components/CV/CVContent.component.tsx
@@ -3,8 +3,6 @@ import Image from "next/image";
 
 import PageHeader from "../UI/PageHeader.component";
 
-
-
 /**
  * Renders CV content
  * Uses React-pdf
@@ -21,23 +19,12 @@ const CVContent = () => (
           <div className="p-4 mx-auto h-96 md:h-full mt-4 flex justify-center items-center">
             <div className="p-4 text-lg rounded">
               <div className="mt-4 flex flex-col items-center md:block">
-                <div className="flex justify-center">
-                  <Image
-                    src="/cv/page_1.webp"
-                    alt="CV"
-                    width={800}
-                    height={1000}
-                    priority
-                  />
+                <div className="flex justify-center hidden md:block">
+                  <Image src="/cv/page_1.webp" alt="CV" width={800} height={1000} priority />
                 </div>
                 <br />
-                <div className="flex justify-center">
-                  <Image
-                    src="/cv/page_2.webp"
-                    alt="CV"
-                    width={800}
-                    height={1000}
-                  />
+                <div className="flex justify-center hidden md:block">
+                  <Image src="/cv/page_2.webp" alt="CV" width={800} height={1000} />
                 </div>
               </div>
               <div className="mx-auto pt-2 text-center sm:mt-2 xs:mt-2 bg-gray-800 rounded w-64 h-12 text-white cursor-pointer">
